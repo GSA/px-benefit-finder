f97091da78796d0c9335dec562619a12b9927a23
gscottqueen <scott.queen@contractor.bixal.com>
2023-04-19 16:05:39 -0400

    including a component guide component

---
+++ bears-app/src/components/app/index.js
@@ -0,0 +1,16 @@
+import { ComponentGuide } from '../index';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>Component Guide</button>
+        <ComponentGuide />
+      </header>
+    </div>
+  );
+}
+
+export default App;

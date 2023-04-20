38b96d1c7fa63f61532cc0f446af19e9c74f4685
gscottqueen <scott.queen@contractor.bixal.com>
2023-04-20 09:28:57 -0400

    import minified uswds css globally

--- bears-app/src/index.js
+++ bears-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components';
 import reportWebVitals from './reportWebVitals';
 
+import '../node_modules/@uswds/uswds/dist/css/uswds.min.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

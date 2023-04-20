0f3259c19b8243e9a0c8ebcf60650a4dbfb0f744
gscottqueen <scott.queen@contractor.bixal.com>
2023-04-20 10:02:50 -0400

    include handleClass with button component

--- bears-app/src/components/button/index.js
+++ bears-app/src/components/button/index.js
@@ -1,6 +1,9 @@
-function Button ({children, onClick}) {
+function Button ({children, onClick, className}) {
+    const handleClassName = className !== undefined ?
+     `usa-button ${className}` : 'usa-button'
+
     return (
-        <button onClick={onClick} className="usa-button">{children}</button>
+        <button onClick={onClick} className={handleClassName}>{children}</button>
     )
 }
 

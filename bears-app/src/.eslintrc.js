a6aefa41808b05cc5b8e84dcab07666c98139a47
gscottqueen <geoffreysqueen@gmail.com>
2023-05-22 12:27:08 -0400

    update lint config

--- bears-app/src/.eslintrc.js
+++ bears-app/src/.eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['react-app', 'standard', 'plugin:prettier/recommended'],
+  extends: [
+    'react-app',
+    'standard',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     'react/prop-types': 'off',
     eqeqeq: [2, 'smart'],

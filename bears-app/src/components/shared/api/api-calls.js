3600a93643caa6177dfa67e04bb19a82b2a94dc5
gscottqueen <geoffreysqueen@gmail.com>
2023-06-07 12:26:46 -0400

    new api fetch export

---
+++ bears-app/src/components/shared/api/api-calls.js
@@ -0,0 +1,29 @@
+/**
+ * an async fetch to get life-event data.
+ * @function
+ * @param {string} lifeEvent - The inherited class from
+ * @return {JSON || Sring} returns JSON data if succesfull
+ */
+export async function GETLifeEvent(lifeEvent) {
+  // get life-event from location
+  if (lifeEvent === undefined) {
+    const location = window.location.pathname
+    lifeEvent = location.substring(location.lastIndexOf('/') + 1)
+  }
+  const response = await fetch(`/bears/api/life-event/${lifeEvent}`)
+    .then(response => {
+      if (response?.ok) {
+        return response.json()
+      }
+      throw new Error(response?.status)
+    })
+    .then(responseJson => {
+      return responseJson
+    })
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.log(error)
+      return 'Something went wrong.'
+    })
+  return response
+}

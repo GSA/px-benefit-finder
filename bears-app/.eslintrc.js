520b1046822349b7432f1b53850fb82ef2c11739
gscottqueen <geoffreysqueen@gmail.com>
2023-06-15 13:42:12 -0400

    init storybook

--- bears-app/.eslintrc.js
+++ bears-app/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:prettier/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:storybook/recommended',
   ],
   rules: {
     'react/prop-types': 'off',

d252736d578f68a9a26743b18d54517d034c05e9
gscottqueen <geoffreysqueen@gmail.com>
2023-06-15 14:41:52 -0400

    global require for jest tests in storybook previews

--- bears-app/.storybook/preview.js
+++ bears-app/.storybook/preview.js
@@ -1,5 +1,14 @@
 /** @type { import('@storybook/react').Preview } */
+import { withTests } from '@storybook/addon-jest';
+import results from '../.jest-test-results.json';
 import "../node_modules/@uswds/uswds/dist/css/uswds.css";
+
+export const decorators = [
+  withTests({
+    results,
+  }),
+];
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },

cc1e171ef228119d79663b99ea57952d49d300f7
FatmaBakir <fatma.bakir@gsa.gov>
2023-06-20 09:30:47 -0400

    Configure circleci pipeline - 26

--- bin/pipeline/initial-setup.sh
+++ bin/pipeline/initial-setup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # get current directory
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
+export CIRCLE_WORKFLOW=$( echo $CIRCLE_WORKFLOW_ID | cut -b -8 )
 
-echo "cloning the repo and initiating the usagov-2021 submodule"
-git clone -b "$CIRCLE_BRANCH" "$CIRCLE_REPOSITORY_URL"
-cd px-bears-drupal
-git submodule init
-git submodule update
 echo "building the bears app in the same step"
 bash ./mv-bears-app.sh
 echo ".................."

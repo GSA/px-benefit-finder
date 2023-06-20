cfbc40fbf4f42b01cbbc77c2d74d727baf411efe
FatmaBakir <fatma.bakir@gsa.gov>
2023-06-20 11:49:38 -0400

    Configure circleci pipeline - 32

--- bin/pipeline/initial-setup.sh
+++ bin/pipeline/initial-setup.sh
@@ -5,9 +5,9 @@ export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null &&
 echo $SCRIPT_DIR
 export CIRCLE_WORKFLOW=$( echo $CIRCLE_WORKFLOW_ID | cut -b -8 )
 
-echo "building the bears app in the same step"
-bash ./mv-bears-app.sh
-echo ".................."
-echo "moving the bears-app module into the usagov-2021 directory"
-mv  usagov_bears usagov-2021/web/modules/custom
-ls -l usagov-2021/web/modules/custom/
+# echo "building the bears app in the same step"
+# bash ./mv-bears-app.sh
+# echo ".................."
+# echo "moving the bears-app module into the usagov-2021 directory"
+# mv  usagov_bears usagov-2021/web/modules/custom
+# ls -l usagov-2021/web/modules/custom/

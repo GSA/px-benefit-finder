d0de3cdf34b1232ffa4d9427c4aceac72035a017
gscottqueen <geoffreysqueen@gmail.com>
2023-06-26 17:52:59 -0400

    update spec heading component

--- bears-app/src/components/shared/Heading/__tests__/index.spec.js
+++ bears-app/src/components/shared/Heading/__tests__/index.spec.js
@@ -3,7 +3,7 @@ import Heading from '../index.jsx'
 
 describe('Heading', () => {
   it('renders a match to the previous snapshot', () => {
-    const component = renderer.create(<Heading />)
+    const component = renderer.create(<Heading headingLevel={1} />)
     expect(component.toJSON()).toMatchSnapshot()
   })
 })

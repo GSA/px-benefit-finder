66c3f40e22a27e73fbf99a1eb9f6f48237e4c2a5
gscottqueen <geoffreysqueen@gmail.com>
2023-06-28 15:20:00 -0400

    export new components

--- bears-app/src/components/shared/index.js
+++ bears-app/src/components/shared/index.js
@@ -2,5 +2,7 @@
 export { default as Button } from './Button'
 export { default as Form } from './Form'
 export { default as Heading } from './Heading'
+export { default as Label } from './Label'
 export { default as Paragraph } from './Paragraph'
+export { default as Radio } from './Radio'
 export { default as Select } from './Select'

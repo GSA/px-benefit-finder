ff76118725bb5983bf710c2837d9f0ef01d520eb
gscottqueen <geoffreysqueen@gmail.com>
2023-06-29 11:50:27 -0400

    updated component documenation

--- bears-app/src/components/shared/Alert/index.jsx
+++ bears-app/src/components/shared/Alert/index.jsx
@@ -1,8 +1,15 @@
 import PropTypes from 'prop-types'
 
+/**
+ * a functional component that renders an information alert
+ * @component
+ * @param {string} children - inherited children
+ * @return {html} returns a wrapped paragraph
+ */
+
 const Alert = ({ children }) => {
   return (
-    <div className="usa-alert usa-alert--info usa-alert--no-icon">
+    <div className="usa-alert usa-alert--info">
       <div className="usa-alert__body">
         <p className="usa-alert__text">{children}</p>
       </div>

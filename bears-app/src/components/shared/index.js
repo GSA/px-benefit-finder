a3216b7f3caeef72b38c38db702ada7f79632ded
gscottqueen <geoffreysqueen@gmail.com>
2023-06-28 21:50:55 -0400

    export our new comoponents form index

--- bears-app/src/components/shared/index.js
+++ bears-app/src/components/shared/index.js
@@ -1,6 +1,9 @@
 // order exports in alphabetical list
 export { default as Button } from './Button'
+export { default as Fieldset } from './Fieldset'
 export { default as Form } from './Form'
 export { default as Heading } from './Heading'
+export { default as Hint } from './Hint'
+export { default as Legend } from './Legend'
 export { default as Paragraph } from './Paragraph'
 export { default as Select } from './Select'

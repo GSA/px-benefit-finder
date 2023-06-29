6750aaff188922142ab1f92cf8fa431e196ed790
gscottqueen <geoffreysqueen@gmail.com>
2023-06-29 11:39:35 -0400

    updated component documentation

--- bears-app/src/components/shared/ObfuscatedLink/index.jsx
+++ bears-app/src/components/shared/ObfuscatedLink/index.jsx
@@ -2,6 +2,17 @@ import { useHandleClassName } from '../hooks/useHandleClassName'
 import PropTypes from 'prop-types'
 import './_index.scss'
 
+/**
+ * a functional component that renders a link as a button
+ * @component
+ * @param {React.ReactNode} children - inherited children
+ * @param {string} className - inherited class(es)
+ * @param {string} href - location
+ * @param {rel} string - follow, nofollow
+ * @param {target} string - new window etc.
+ * @param {ext} boolean - is the link an external link
+ * @return {html} returns a semantic html anchor element
+ */
 const ObfuscatedLink = ({ children, className, href, rel, target, ext }) => {
   const defaultClasses = ext
     ? ['usa-button', 'usa-link--external', 'obfuscated-link']

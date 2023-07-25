df4bd760beeb3ee4af4329bac0a923bd78198d84
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-07-25 15:15:54 -0400

    init card component

---
+++ bears-app/src/shared/components/Card/index.stories.jsx
@@ -0,0 +1,14 @@
+import Card from './index.jsx'
+
+export default {
+  component: Card,
+  tags: ['autodocs'],
+  args: {
+    className: 'relative-benefit-card',
+    title: 'Header Content',
+    body: 'BodyContent',
+    link: 'https://www.google.com',
+  },
+}
+
+export const Primary = {}

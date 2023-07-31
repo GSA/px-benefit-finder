f3f8cbd9a727b11e39509f6f80b7794334843b13
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-07-31 11:52:45 -0400

    include language context

--- bears-app/src/App/index.jsx
+++ bears-app/src/App/index.jsx
@@ -1,44 +1,58 @@
-import { useState } from 'react'
-import * as apiCalls from '../shared/api/api-calls'
+import { useState, createContext } from 'react'
+// import * as apiCalls from '../shared/api/api-calls'
+import * as en from '../shared/locales/en/en.json'
+import * as es from '../shared/locales/es/es.json'
 
 /**
  * a functional component that renders our application.
  * @component
  */
 function App() {
-  // state
+  const LanguageContext = createContext({ en, es })
 
-  /**
-   * lazy load our data state.
-   * @function
-   * @param {promise} setData - the state of environment
-   * @return {state} returns null if not set
-   */
-  const [data, setData] = useState(() => {
-    apiCalls.GETLifeEvent().then(response => setData(response))
-  })
+  // /**
+  //  * lazy load our data state.
+  //  * @function
+  //  * @param {promise} setData - the state of environment
+  //  * @return {state} returns null if not set
+  //  */
+  // const [data, setData] = useState(() => {
+  //   apiCalls.GETLifeEvent().then(response => setData(response))
+  // })
 
   // handlers
 
-  /**
-   * handle our data maping.
-   * @function
-   * @param {object} data - the json object returned from data load
-   * @return {string} returns strigified data
-   */
-  const handleData = data => {
-    if (data === undefined) {
-      return 'loading...'
-    }
-    // either return the mapped data object or the error
-    return data?.data ? data.data.map(d => JSON.stringify(d)) : data
+  // /**
+  //  * handle our data maping.
+  //  * @function
+  //  * @param {object} data - the json object returned from data load
+  //  * @return {string} returns strigified data
+  //  */
+  // const handleData = data => {
+  //   if (data === undefined) {
+  //     return 'loading...'
+  //   }
+  //   // either return the mapped data object or the error
+  //   return data?.data ? data.data.map(d => JSON.stringify(d)) : data
+  // }
+
+  const handleLanguage = () => {
+    const string = /^\/es/
+    return string.test(window.location.pathname) ? es : en
   }
 
+  // state
+  const [t] = useState(handleLanguage)
+
   return (
-    <div className="bears-app">
-      <h1>BEARS Hello World!</h1>
-      {handleData(data)}
-    </div>
+    <LanguageContext.Provider value={t}>
+      {console.log(t)}
+      <div className="bears-app">
+        <h1>BEARS Hello World!</h1>
+        {/* {handleData(data)} */}
+        <div>{t && t.intro.button}</div>
+      </div>
+    </LanguageContext.Provider>
   )
 }
 

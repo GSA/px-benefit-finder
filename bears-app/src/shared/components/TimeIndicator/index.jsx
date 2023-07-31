3f8822eb9086de1bd849002d9572375795b3c831
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-07-31 13:50:00 -0400

    update time indicator component to include description as prop

--- bears-app/src/shared/components/TimeIndicator/index.jsx
+++ bears-app/src/shared/components/TimeIndicator/index.jsx
@@ -4,14 +4,20 @@ import './_index.scss'
 /**
  * a functional component that renders a div and expects a string description of time
  * @component
+ * @param {string} description - translated ui values to describe time estimate component
  * @param {string} timeEstimate - The inherited value for time description
  * @return {html} returns a div
  */
-const TimeIndicator = ({ timeEstimate }) => {
-  return <div className="time-indicator">Time to complete: {timeEstimate}</div>
+const TimeIndicator = ({ timeEstimate, description }) => {
+  return (
+    <div className="time-indicator">
+      {description} {timeEstimate}
+    </div>
+  )
 }
 
 TimeIndicator.propTypes = {
+  description: PropTypes.string,
   timeEstimate: PropTypes.string,
 }
 

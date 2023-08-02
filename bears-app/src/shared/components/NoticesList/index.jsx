a992f90a0733bef7d2656e9ab7679eb0a7fac330
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-08-02 12:32:40 -0400

    updated docs and stories for NoticesList component

--- bears-app/src/shared/components/NoticesList/index.jsx
+++ bears-app/src/shared/components/NoticesList/index.jsx
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types'
 import createMarkup from '../../utils/createMarkup'
 
+/**
+ * a functional component that renders a list of information
+ * @component
+ * @param {array} data inherited ui translations
+ * @return {html} returns a semantic ul
+ */
 const NoticesList = ({ data }) => {
+  /**
+   * a functional component that renders a link as a button
+   * @component
+   * @param {object} data inherited ui translations
+   * @return {html} returns a semantic li
+   */
   const Notices = data => {
     return (
       data &&
--- bears-app/src/shared/components/NoticesList/index.stories.jsx
+++ bears-app/src/shared/components/NoticesList/index.stories.jsx
@@ -1,8 +1,12 @@
 import NoticesList from './index.jsx'
+import * as en from '../../locales/en/en.json'
 
 export default {
   component: NoticesList,
   tags: ['autodocs'],
+  args: {
+    data: en.intro.notices.list,
+  },
 }
 
 export const Primary = {}

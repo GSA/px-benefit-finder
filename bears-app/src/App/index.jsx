3d8f79e192bbf83a9035de231abcea70024cedb0
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-08-22 14:29:22 -0400

    include a fetch and make sure it loads

--- bears-app/src/App/index.jsx
+++ bears-app/src/App/index.jsx
@@ -1,4 +1,5 @@
-import { useState, createContext } from 'react'
+import { useState, createContext, useEffect } from 'react'
+import * as apiCalls from '../shared/api/api-calls'
 import {
   Intro,
   LifeEventSection,
@@ -12,8 +13,7 @@ import './_index.scss'
 // data and ui content
 import * as en from '../shared/locales/en/en.json'
 import * as es from '../shared/locales/es/es.json'
-// import content from '../shared/api/mock-data/content-data'
-import content from '../shared/api/mock-data/current.js'
+// import content from '../shared/api/mock-data/current.js'
 
 /**
  * a functional component that renders our application.
@@ -22,6 +22,16 @@ import content from '../shared/api/mock-data/current.js'
 function App() {
   // we create context state to provide translations for our two languages
   const LanguageContext = createContext({ en, es })
+
+  /**
+   * lazy load our data state.
+   * @function
+   * @param {promise} setData - the state of environment
+   * @return {state} returns null if not set
+   */
+  const [content, setContent] = useState(() => {
+    apiCalls.GETLifeEvent().then(response => setContent(response.data))
+  })
 
   // handlers
   /**
@@ -34,82 +44,86 @@ function App() {
     return string.test(window.location.pathname) ? es : en
   }
 
-  // destructure data
-  const { data } = JSON.parse(content)
-  const { lifeEventForm, benefits } = data
-
   // set data state
-  const [stepDataArray, setStepDataArray] = useState([
-    ...lifeEventForm.sectionsEligibilityCriteria,
-  ])
-  const [benfitsArray, setBenefitsArray] = useState([...benefits])
+  const [stepDataArray, setStepDataArray] = useState()
+  const [benfitsArray, setBenefitsArray] = useState()
+
+  useEffect(() => {
+    content && setBenefitsArray([...content.benefits])
+    content &&
+      setStepDataArray([...content.lifeEventForm.sectionsEligibilityCriteria])
+  }, [content])
 
   // state
   const [t] = useState(handleLanguage) // tranlations
   const [step, setStep] = useState(0) // steps indicator
-  const [stepData, setStepData] = useState(stepDataArray[step]) // content
+  const [stepData, setStepData] = useState(
+    () => stepDataArray && stepDataArray[step]
+  ) // content
   const [verifyStep, setVerifyStep] = useState(false) // verification view
   const [viewResults, setViewResults] = useState(false) // resuts view
 
   return (
-    <LanguageContext.Provider value={t}>
-      <div
-        className={`bears-app ${
-          step !== 0 && viewResults !== true ? 'form' : ''
-        }`}
-      >
-        {step === 0 ? (
-          <Intro
-            data={lifeEventForm}
-            ui={t.intro}
-            setStep={setStep}
-            step={step}
-          />
-        ) : viewResults === true ? (
-          <ResultsView
-            stepDataArray={stepDataArray}
-            data={benfitsArray}
-            setBenefitsArray={() => setBenefitsArray()}
-            ui={t.resultsView}
-            handleStepBack={() => {
-              setVerifyStep(false)
-              setViewResults(false)
-            }}
-          />
-        ) : verifyStep === false ? (
-          <div>
-            <Form>
-              <LifeEventSection
-                step={step}
-                setStep={setStep}
-                data={stepDataArray}
-                handleData={() => setStepDataArray()}
-                stepData={stepData}
-                setStepData={setStepData}
-                verifyStep={verifyStep}
-                setVerifyStep={() => setVerifyStep(true)}
-                setViewResults={() => setViewResults(true)}
-                ui={t}
-              />
-            </Form>
-          </div>
-        ) : (
-          <VerifySelectionsView
-            handleStepBack={() => {
-              setVerifyStep(false)
-              setViewResults(false)
-            }}
-            handleStepForward={() => {
-              setViewResults(true)
-            }}
-            ui={t}
-            data={stepDataArray}
-            step={step}
-            setStep={setStep}
-          />
-        )}
-      </div>
-    </LanguageContext.Provider>
+    content && (
+      <LanguageContext.Provider value={t}>
+        <div
+          className={`bears-app ${
+            step !== 0 && viewResults !== true ? 'form' : ''
+          }`}
+        >
+          {step === 0 ? (
+            <Intro
+              data={content.lifeEventForm}
+              ui={t.intro}
+              setStep={setStep}
+              step={step}
+            />
+          ) : viewResults === true ? (
+            <ResultsView
+              stepDataArray={stepDataArray}
+              data={benfitsArray}
+              setBenefitsArray={() => setBenefitsArray()}
+              ui={t.resultsView}
+              handleStepBack={() => {
+                setVerifyStep(false)
+                setViewResults(false)
+              }}
+            />
+          ) : verifyStep === false ? (
+            <div>
+              <Form>
+                <LifeEventSection
+                  step={step}
+                  setStep={setStep}
+                  data={stepDataArray}
+                  handleData={() => setStepDataArray()}
+                  stepData={stepData}
+                  setStepData={setStepData}
+                  verifyStep={verifyStep}
+                  setVerifyStep={() => setVerifyStep(true)}
+                  setViewResults={() => setViewResults(true)}
+                  ui={t}
+                />
+              </Form>
+            </div>
+          ) : (
+            <VerifySelectionsView
+              handleStepBack={() => {
+                setVerifyStep(false)
+                setViewResults(false)
+              }}
+              handleStepForward={() => {
+                setViewResults(true)
+              }}
+              ui={t}
+              data={stepDataArray}
+              step={step}
+              setStep={setStep}
+            />
+          )}
+        </div>
+      </LanguageContext.Provider>
+    )
   )
 }
 

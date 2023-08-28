eb4de425cc59ca876950142dd37bc6b95503b120
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-08-28 12:55:37 -0400

    break apart date into functional components

--- bears-app/src/shared/components/Date/index.jsx
+++ bears-app/src/shared/components/Date/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import parseDate from '../../utils/parseDate'
+// import { useHandleClassName } from '../../hooks/useHandleClassName'
 import './_index.scss'
 
 /**
@@ -15,25 +16,47 @@ const Date = ({ onChange, value, required, ui }) => {
   const { labelDay, labelMonth, labelYear, monthOptions } = date
   const { dateDefaultValue } = select
 
-  return (
-    <div className="usa-memorable-date">
+  // date input values
+  // if no value is inherited create a new value
+  // or
+  // if a value is inherited as an object, return the value
+  // or
+  // if there is no value, default to an empty string
+  // in the case of date we wait until the value from onChange is a 4 digit value
+  const monthValue =
+    new window.Date(parseDate(value)).getMonth() || (value && value.month) || ''
+  const dayValue =
+    new window.Date(parseDate(value)).getDate() || (value && value.day) || ''
+  const yearValue =
+    (value?.year?.length === 4 &&
+      new window.Date(parseDate(value)).getFullYear()) ||
+    (value && value.year) ||
+    ''
+
+  // const defaultClasses =
+  //   required === 'TRUE'
+  //     ? ['usa-alert', 'usa-alert--error', 'display-none']
+  //     : [
+  //         'usa-alert',
+  //         'usa-alert--info',
+  //         `${noBackground ? 'no-background' : ''}`,
+  //       ]
+
+  const handleRequired = required === 'TRUE' ? 'required-field' : ''
+
+  const MonthSelect = () => {
+    return (
       <div className="usa-form-group usa-form-group--month usa-form-group--select">
         <label className="usa-label" htmlFor="date_of_birth_month">
           {labelMonth}
         </label>
         <select
-          className={`usa-select ${
-            required === 'TRUE' ? 'required-field' : ''
-          }`}
+          className={`usa-select ${handleRequired}`}
           id="date_of_birth_month"
           name="date_of_birth_month"
           aria-describedby="mdHint"
           required="required"
-          value={
-            new window.Date(parseDate(value)).getMonth() ||
-            (value && value.month) ||
-            ''
-          }
+          value={monthValue}
           onChange={onChange}
         >
           <option value="" key="default" disabled>
@@ -46,32 +69,38 @@ const Date = ({ onChange, value, required, ui }) => {
           ))}
         </select>
       </div>
+    )
+  }
+
+  const DayInput = () => {
+    return (
       <div className="usa-form-group usa-form-group--day">
         <label className="usa-label" htmlFor="date_of_birth_day">
           {labelDay}
         </label>
         <input
-          className={`usa-input ${required === 'TRUE' ? 'required-field' : ''}`}
+          className={`usa-input $${handleRequired}`}
           aria-describedby="mdHint"
           id="date_of_birth_day"
           name="date_of_birth_day"
           maxLength="2"
           pattern="[0-9]*"
           inputMode="numeric"
-          value={
-            new window.Date(parseDate(value)).getDate() ||
-            (value && value.day) ||
-            ''
-          }
+          value={dayValue}
           onChange={onChange}
         />
       </div>
+    )
+  }
+
+  const YearInput = () => {
+    return (
       <div className="usa-form-group usa-form-group--year">
         <label className="usa-label" htmlFor="date_of_birth_year">
           {labelYear}
         </label>
         <input
-          className={`usa-input ${required === 'TRUE' ? 'required-field' : ''}`}
+          className={`usa-input ${handleRequired}`}
           aria-describedby="mdHint"
           id="date_of_birth_year"
           name="date_of_birth_year"
@@ -79,15 +108,18 @@ const Date = ({ onChange, value, required, ui }) => {
           maxLength="4"
           pattern="[0-9]*"
           inputMode="numeric"
-          value={
-            (value?.year?.length === 4 &&
-              new window.Date(parseDate(value)).getFullYear()) ||
-            (value && value.year) ||
-            ''
-          }
+          value={yearValue}
           onChange={onChange}
         />
       </div>
+    )
+  }
+
+  return (
+    <div className="usa-memorable-date">
+      <MonthSelect />
+      <DayInput />
+      <YearInput />
     </div>
   )
 }

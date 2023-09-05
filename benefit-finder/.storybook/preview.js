c4ecd229963c7b3095721deb3daef3648cc5f181
scottqueen-bixal <scott.queen@contractor.bixal.com>
2023-09-05 11:07:04 -0400

    import usagov css styles

--- benefit-finder/.storybook/preview.js
+++ benefit-finder/.storybook/preview.js
@@ -4,6 +4,7 @@ import results from '../.jest-test-results.json'
 // pull in our uswds dependencies
 import '../node_modules/@uswds/uswds/dist/js/uswds.js'
 import '../node_modules/@uswds/uswds/dist/css/uswds.css'
+import '../themes/custom/usagov/css/styles.css'
 import '../build/static/css/benefit-finder.min.css'
 
 const customViewports = {

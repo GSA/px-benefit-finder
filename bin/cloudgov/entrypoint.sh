19f690b701d21c4ee65a6338bb1f00438c1a5696
FatmaBakir <fatma.bakir@gsa.gov>
2023-11-29 17:09:23 -0500

    [598] - use entrypoint.sh 37

--- bin/cloudgov/entrypoint.sh
+++ bin/cloudgov/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 /var/www/vendor/bin/drush state:set system.maintenance_mode 0
+/var/www/vendor/bin/drush pm:uninstall usagov_login || true
 /var/www/vendor/bin/drush pm:enable usagov_benefit_finder_content
 /var/www/vendor/bin/drush pm:enable usagov_benefit_finder_api
 /var/www/vendor/bin/drush pm:enable usagov_benefit_finder

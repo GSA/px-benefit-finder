5ffce33b22ea2ebb02e895278ab9f814946e9f23
FatmaBakir <fatma.bakir@gsa.gov>
2023-11-30 10:40:42 -0500

    [598] - Enhance pipeline 4

--- bin/cloudgov/entrypoint.sh
+++ bin/cloudgov/entrypoint.sh
@@ -2,13 +2,5 @@
 
 /var/www/vendor/bin/drush state:set system.maintenance_mode 0
 /var/www/vendor/bin/drush pm:uninstall usagov_login || true
-/var/www/vendor/bin/drush pm:enable usagov_benefit_finder_content
-/var/www/vendor/bin/drush pm:enable usagov_benefit_finder_api
-/var/www/vendor/bin/drush pm:enable usagov_benefit_finder
-/var/www/vendor/bin/drush pm:enable usagov_benefit_finder_page
-/var/www/vendor/bin/drush cr
-cd /var/www/web/
-mkdir -p content/sync
-chown -R nginx:nginx content
 /var/www/vendor/bin/drush cr
 /init &

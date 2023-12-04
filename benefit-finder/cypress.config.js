0eba76db3f3bea8f2475e0c7eccc9528c4cbcffc
FatmaBakir <fatma.bakir@gsa.gov>
2023-12-04 13:50:27 -0500

    [612] - Integrate cypress testing into release-action workflow 38

--- benefit-finder/cypress.config.js
+++ benefit-finder/cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:6006',
+    env: {
+      authUsername: $storybook_username,
+      authPassword: storybook_password,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

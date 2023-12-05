2c1dddf0a32251e9f5e18f836387d36263f208e3
FatmaBakir <fatma.bakir@gsa.gov>
2023-12-05 14:10:19 -0500

    [612] - Integrate cypress testing into release-action workflow 54

--- benefit-finder/cypress/e2e/death-of-a-loved-one/error-message-display.cy.js
+++ benefit-finder/cypress/e2e/death-of-a-loved-one/error-message-display.cy.js
@@ -4,7 +4,12 @@ import { pageObjects } from '../../support/pageObjects'
 
 describe('Validate correct error messages display for negative scenarios', () => {
   it('Should display error message when user does not enter date of birth required field', () => {
-    cy.visit('/iframe.html?args=&id=app--primary&viewMode=story')
+    cy.visit('/iframe.html?args=&id=app--primary&viewMode=story', {
+      auth: {
+          username: Cypress.env("authUsername"),
+          password: Cypress.env("authPassword"),
+      },
+    }) 
     pageObjects.button().contains('Start').click()
     pageObjects.button().contains('Continue').click()
     pageObjects.benefitSectionAlert().should('contain.text', 'Error status')

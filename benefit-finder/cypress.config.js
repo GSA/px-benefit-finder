488940dee5275f0b0346a9902ef27a7140579cef
FatmaBakir <fatma.bakir@gsa.gov>
2023-12-05 14:50:05 -0500

    [612] - Integrate cypress testing into release-action workflow 59

--- benefit-finder/cypress.config.js
+++ benefit-finder/cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: '$CYPRESS_baseUrl',
     env: {
-      authUsername: '$storybook_username',
-      authPassword: '$storybook_password',
+      authUsername: '$CYPRESS_username',
+      authPassword: '$CYPRESS_password',
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- benefit-finder/cypress/e2e/death-of-a-loved-one/error-message-display.cy.js
+++ benefit-finder/cypress/e2e/death-of-a-loved-one/error-message-display.cy.js
@@ -6,10 +6,10 @@ describe('Validate correct error messages display for negative scenarios', () =>
   it('Should display error message when user does not enter date of birth required field', () => {
     cy.visit('/iframe.html?args=&id=app--primary&viewMode=story', {
       auth: {
-          username: Cypress.env('authUsername'),
-          password: Cypress.env('authPassword'),
+        username: Cypress.env('authUsername'),
+        password: Cypress.env('authPassword'),
       },
-    }) 
+    })
     pageObjects.button().contains('Start').click()
     pageObjects.button().contains('Continue').click()
     pageObjects.benefitSectionAlert().should('contain.text', 'Error status')

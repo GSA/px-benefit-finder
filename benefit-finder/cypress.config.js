b80b06819a76631a36aa7c4d50244ad93761a466
FatmaBakir <fatma.bakir@gsa.gov>
2023-12-05 10:29:42 -0500

    [612] - Integrate cypress testing into release-action workflow 45

--- benefit-finder/cypress.config.js
+++ benefit-finder/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     baseUrl: "$CYPRESS_baseUrl",
     env: {
       authUsername: "$storybook_username",
-      authPassword: "$storybook_password"
+      authPassword: "$storybook_password",
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ benefit-finder/cypress/support/basicauth-login.spec.js
@@ -0,0 +1,25 @@
+// your_spec.js
+
+describe('Basic Auth Test', () => {
+    it('should authenticate with Basic Auth', () => {
+      // If using the Cypress Credentials Plugin
+      cy.getCredentials('yourWebsite').then(credentials => {
+        const { username, password } = credentials;
+  
+        // Perform Basic Auth using cy.request
+        cy.request({
+          method: 'GET',
+          url: "$CYPRESS_baseUrl",
+          auth: {
+            $storybook_username,
+            $storybook_password,
+          },
+        }).then(response => {
+          // Validate that the request was successful
+          expect(response.status).to.eq(200);
+  
+          // Add your additional assertions if needed
+        });
+      });
+    });
+  });

8896f0ea0392ac7143af3568313825a5b7be4b9c
Nehemiah Abuga <121119072+nehemiah-abuga@users.noreply.github.com>
2023-12-05 19:25:29 -0700

    add auth for storybook tests

--- benefit-finder/cypress.config.js
+++ benefit-finder/cypress.config.js
@@ -1,8 +1,17 @@
 const { defineConfig } = require('cypress')
 
+const authData = {
+  authUsername: '{replace-with-auth-username}',
+  authPassword: '{replace-with-auth-password}',
+}
+
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:6006',
+    baseUrl: `http://${authData.authPassword}-${authData.authUsername}-dev.app.cloud.gov`,
+    env: {
+      authUsername: authData.authUsername,
+      authPassword: authData.authPassword,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

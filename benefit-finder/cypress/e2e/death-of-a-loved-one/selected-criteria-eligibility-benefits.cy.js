ece1cd5f20398a230e8159fe9f27e88677a27d76
Nehemiah Abuga <121119072+nehemiah-abuga@users.noreply.github.com>
2023-12-13 10:48:50 -0700

    add validations for scenario 1

--- benefit-finder/cypress/e2e/death-of-a-loved-one/selected-criteria-eligibility-benefits.cy.js
+++ benefit-finder/cypress/e2e/death-of-a-loved-one/selected-criteria-eligibility-benefits.cy.js
@@ -3,8 +3,38 @@
 import { pageObjects } from '../../support/pageObjects'
 import * as utils from '../../support/utils'
 import * as EN_DOLO_MOCK_DATA from '../../../../benefit-finder/src/shared/api/mock-data/current.json'
+import * as BENEFITS_ELIBILITY_DATA from '../../fixtures/benefits-eligibility.json'
 
 describe('Validate correct eligibility benefits display based on selected criteria/options', () => {
+  it.only('qa scenario 1 - Verify correct benefit results for query values in search parameter of URL', () => {
+    const benefitsCriteria = BENEFITS_ELIBILITY_DATA.Scenario_1.en.param
+    const applicant_date_of_birth = encodeURI(
+      `{"month":"${
+        benefitsCriteria.applicant_date_of_birth_month - 1
+      }","day":"${benefitsCriteria.applicant_date_of_birth_day}","year":"${
+        benefitsCriteria.applicant_date_of_birth_year
+      }"}`
+    )
+
+    const deceased_date_of_death = encodeURI(
+      `{"month":"${benefitsCriteria.deceased_date_of_death_month - 1}","day":"${
+        benefitsCriteria.deceased_date_of_death_day
+      }","year":"${benefitsCriteria.deceased_date_of_death_year}"}`
+    )
+    cy.visit(
+      `/death?applicant_date_of_birth=${applicant_date_of_birth}&applicant_relationship_to_the_deceased=${benefitsCriteria.applicant_relationship_to_the_deceased}&applicant_marital_status=${benefitsCriteria.applicant_marital_status}&applicant_citizen_status=${benefitsCriteria.applicant_citizen_status}&applicant_care_for_child=${benefitsCriteria.applicant_care_for_child}&applicant_paid_funeral_expenses=${benefitsCriteria.applicant_paid_funeral_expenses}&deceased_date_of_death=${deceased_date_of_death}&deceased_death_location_is_US=${benefitsCriteria.deceased_death_location_is_US}&deceased_paid_into_SS=${benefitsCriteria.deceased_paid_into_SS}&deceased_public_safety_officer=${benefitsCriteria.deceased_public_safety_officer}&deceased_miner=${benefitsCriteria.deceased_miner}&deceased_american_indian=${benefitsCriteria.deceased_american_indian}&deceased_died_of_COVID=${benefitsCriteria.deceased_died_of_COVID}&deceased_served_in_active_military=${benefitsCriteria.deceased_served_in_active_military}&shared=true`
+    )
+
+    pageObjects
+      .benefitsAccordion()
+      .filter(':visible')
+      .should(
+        'have.length',
+        BENEFITS_ELIBILITY_DATA.Scenario_1.en.results.likely_eligible.length
+      )
+      .and('contain', 'Likely Eligible')
+  })
+
   it('Should render Survivor Benefits for Child benefit accordion correctly based on selected cretiria options', () => {
     // 18 years ago minus one day - applicant under 18 years old
     // 1 day = 365.2425 (accounts for leap year)

90dd961e1b44b1eb2c997bd4c996a757c15a2518
FatmaBakir <fatma.bakir@gsa.gov>
2024-01-10 18:11:46 -0500

    [657] - configure static site building - new approach-35

--- bin/cloudgov/entrypoint.sh
+++ bin/cloudgov/entrypoint.sh
@@ -3,4 +3,5 @@
 /var/www/vendor/bin/drush state:set system.maintenance_mode 0
 /var/www/vendor/bin/drush pm:uninstall usagov_login || true
 /var/www/vendor/bin/drush cr
+/var/www/vendor/bin/drush sdel usagov.tome_run_disabled
 /init &

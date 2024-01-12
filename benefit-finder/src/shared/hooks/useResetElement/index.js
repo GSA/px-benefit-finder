e5cec91f41ee1addabce57c7f26097812851d37b
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-01-12 13:38:34 -0500

    move hooks to index export

--- benefit-finder/src/shared/hooks/useResetElement/index.js
+++ benefit-finder/src/shared/hooks/useResetElement/index.js
@@ -1,18 +1,29 @@
 import { useRef, useEffect } from 'react'
 
-export const useResetElement = () => {
+const useResetElement = () => {
   /**
    * a hook to set and get the reset element to focus
    * @function
-   * @return {ref} returns ref element
+   * @return {React.ref} returns ref element
    */
+
+  // example useage
+  // assign hook to const
+
+  // const resetElement = useResetElement()
+
+  // foucus element as needed in an effect or synthetic event
+
+  // useEffect(() => {
+  //   resetElement.current?.focus()
+  // }, [resetElement])
 
   // create a sr-only element to set focus on that is not in the default tab order
   useEffect(() => {
     if (document.getElementById('index-reset') === null) {
       document.body.insertAdjacentHTML(
         'beforebegin',
-        '<span tabIndex="-1" id="index-reset" class="a11y-sr-only"/>'
+        '<span tabIndex="-1" id="index-reset" data-testid="index-reset" class="a11y-sr-only"/>'
       )
     }
   }, [])
@@ -20,3 +31,5 @@ export const useResetElement = () => {
   const resetElement = useRef(document.getElementById('index-reset'))
   return resetElement
 }
+
+export default useResetElement

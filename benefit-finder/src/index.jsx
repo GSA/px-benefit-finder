4c483574e27f4fb37b44d163a898bac32f0fea09
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-01-29 13:24:32 -0500

    remove web vitals

--- benefit-finder/src/index.jsx
+++ benefit-finder/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 // a version of uswds will already be on production
 if (process.env.NODE_ENV !== 'production') {
@@ -15,8 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()

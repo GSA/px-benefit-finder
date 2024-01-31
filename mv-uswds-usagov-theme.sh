8d7f52d841ceb837e0a777cd3538c2369afdd5ca
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-01-31 14:56:22 -0500

    update move theme to check for theme first

--- mv-uswds-usagov-theme.sh
+++ mv-uswds-usagov-theme.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# this file checks to ensure the same version of uswds from usagov is installed in our benefit-finder application
+# this file moves the usa git submodule uswds custom theme into our application directory so storybook can access it
 
 # get current directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# define package file
-readonly PROJECT_PACKAGE_CONFIG_FILE="package.json"
-
 # benefit-finder app location
-readonly BENEFIT_FINDER_PROJECT_LOCATION="${SCRIPT_DIR}/benefit-finder/themes/"
+readonly BENEFIT_FINDER_PROJECT_LOCATION="${SCRIPT_DIR}/benefit-finder"
+readonly STORYBOOK_THEMES_DIR="${BENEFIT_FINDER_PROJECT_LOCATION}/themes"
 
 # USAGOV-2021 project
 readonly USAGOV_PROJECT_LOCATION="${SCRIPT_DIR}/usagov-2021/"
@@ -16,5 +14,16 @@ readonly USAGOV_PROJECT_THEME_LOCATION="${USAGOV_PROJECT_LOCATION}web/themes/cus
 readonly USAGOV_PROJECT_THEME_DIR="${USAGOV_PROJECT_LOCATION}web/themes"
 
 
-cd $USAGOV_PROJECT_THEME_LOCATION && npm install && npm run build
-cp -r $USAGOV_PROJECT_THEME_DIR $BENEFIT_FINDER_PROJECT_LOCATION
+if [ -d $USAGOV_PROJECT_THEME_LOCATION ]
+then
+  cd $USAGOV_PROJECT_THEME_LOCATION && npm install && npm run build
+  if [ -d $STORYBOOK_THEMES_DIR ]
+  then
+    echo "Directory $STORYBOOK_THEMES_DIR exists."
+    cp -r $USAGOV_PROJECT_THEME_DIR $BENEFIT_FINDER_PROJECT_LOCATION
+  else
+    cp -r $USAGOV_PROJECT_THEME_DIR $STORYBOOK_THEMES_DIR
+  fi
+else
+  echo "Missing git submodule theme"
+fi

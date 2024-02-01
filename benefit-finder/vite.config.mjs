c05eba9c79380b4d61c3dc3a44142c96279ec1f0
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-02-01 11:12:58 -0500

    import dist config

--- benefit-finder/vite.config.mjs
+++ benefit-finder/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 import copy from 'rollup-plugin-copy'
+import distTargets from './dist.config'
 
 const envLocal = loadEnv('all', process.cwd())
 const proxyURL = envLocal.VITE_PROXY_URL
@@ -29,71 +30,7 @@ export default defineConfig({
     react(),
     eslint(),
     copy({
-      targets: [
-        {
-          src: ['src/*', '!src/setupTests.js'],
-          dest: 'dist',
-          expandDirectories: true,
-          onlyFiles: true,
-        },
-        {
-          src: ['package.json', 'package-lock.json'],
-          dest: 'dist',
-          expandDirectories: true,
-          onlyFiles: true,
-        },
-        {
-          src: ['src/App/*', '!src/App/*.stories.jsx'],
-          dest: 'dist/src',
-          expandDirectories: true,
-          onlyFiles: true,
-        },
-        {
-          src: 'src/shared/api/*',
-          dest: 'dist/src/',
-          expandDirectories: true,
-          onlyFiles: true,
-        },
-        {
-          src: 'src/shared/locales',
-          dest: 'dist/src/',
-        },
-        {
-          src: [
-            'src/shared/hooks/*',
-            'src/shared/hooks/**/*',
-            '!src/shared/hooks/**/**/__tests__',
-          ],
-          dest: 'dist/src/',
-          expandDirectories: true,
-          onlyFiles: true,
-        },
-        {
-          src: 'src/shared/styles',
-          dest: 'dist/src/',
-        },
-        {
-          src: [
-            'src/shared/utils/*',
-            'src/shared/utils/**/*',
-            '!src/shared/utils/**/**/__tests__',
-          ],
-          dest: 'dist/src/',
-          expandDirectories: true,
-          onlyFiles: true,
-        },
-        {
-          src: [
-            'src/shared/components/*',
-            'src/shared/components/**/*',
-            '!src/shared/components/**/**/__tests__',
-            '!src/shared/components/**/index.stories.jsx',
-          ],
-          dest: 'dist/src/',
-          expandDirectories: true,
-          onlyFiles: true,
-        },
-      ],
+      targets: distTargets,
       flatten: false,
       hook: 'writeBundle',
     }),

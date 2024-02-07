6e1fa3eac8516e7a0fedb9972dcf15f542c8127b
gscottqueen <scott.queen@contractor.bixal.com>
2024-02-07 14:22:26 -0500

    import all the styles during the dev server

--- benefit-finder/src/index.jsx
+++ benefit-finder/src/index.jsx
@@ -4,8 +4,10 @@ import App from './App'
 
 // a version of uswds will already be on production
 if (process.env.NODE_ENV !== 'production') {
-  import('../node_modules/@uswds/uswds/dist/js/uswds.js')
-  import('../node_modules/@uswds/uswds/dist/css/uswds.css')
+  import('../node_modules/@uswds/uswds/dist/js/uswds.js') // uswds
+  import('../node_modules/@uswds/uswds/dist/css/uswds.css') // uswds
+  import('../themes/custom/usagov/css/styles.css') // usagov
+  import('../build/assets/benefit-finder.min.css') // benefit-finder
 }
 
 const root = ReactDOM.createRoot(document.getElementById('benefit-finder'))

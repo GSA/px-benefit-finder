391121da2b2492d3f27fab40752f8d679e21b537
gscottqueen <scott.queen@contractor.bixal.com>
2024-02-07 16:32:35 -0500

    remove benfit finder compiled css for vite dev

--- benefit-finder/src/index.jsx
+++ benefit-finder/src/index.jsx
@@ -7,7 +7,6 @@ if (process.env.NODE_ENV !== 'production') {
   import('../node_modules/@uswds/uswds/dist/js/uswds.js') // uswds
   import('../node_modules/@uswds/uswds/dist/css/uswds.css') // uswds
   import('../themes/custom/usagov/css/styles.css') // usagov
-  import('../build/assets/benefit-finder.min.css') // benefit-finder
 }
 
 const root = ReactDOM.createRoot(document.getElementById('benefit-finder'))

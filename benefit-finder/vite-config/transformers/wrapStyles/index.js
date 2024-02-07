7a84259678bbb31d8c54bf1b2e4df4c3fcfa6635
gscottqueen <scott.queen@contractor.bixal.com>
2024-02-07 14:22:07 -0500

    vite plugin to wrap our styles with an id on build

--- benefit-finder/vite-config/transformers/wrapStyles/index.js
+++ benefit-finder/vite-config/transformers/wrapStyles/index.js
@@ -1,17 +1,30 @@
-import { extname } from 'path'
+import { writeFileSync } from 'fs';
 
 export default function wrapStyles(options) {
-  console.log(options)
+
+  const {id} = options
+
   return {
-    name: 'wrap-styles',
-    renderChunk(code, chunkInfo) {
-      const isStyleChunk =
-        chunkInfo.facadeModuleId && extname(chunkInfo.facadeModuleId) === '.css'
-      if (isStyleChunk) {
-        const wrappedCode = `#${options.id} { ${code} }`
-        console.log(code)
-        return { code: wrappedCode }
+    name: 'wrap-css',
+    writeBundle(options, bundle) {
+      // Check if there is any CSS output in the bundle
+      if (bundle) {
+        console.log(Object.keys(bundle))
+        // Assuming only one CSS file is generated, you can modify this according to your project structure
+        const cssFileName = Object.keys(bundle).filter(file => file.includes('.min.css'));
+        const cssContent = bundle[cssFileName].source;
+
+        // Remove charset
+        const trimmedCSS = cssContent.replace(/^@charset "UTF-8";/, '')
+        
+        // Wrap the CSS content with the provided ID, and charset
+        const wrappedCssContent = `@charset "UTF-8"; #${id} { ${trimmedCSS} }`;
+        bundle[cssFileName].source = wrappedCssContent
+
+        // Write the wrapped CSS content back to the file
+        writeFileSync(`./build/${cssFileName}`, wrappedCssContent);
+        return bundle
       }
-    },
-  }
+    }
+  };
 }

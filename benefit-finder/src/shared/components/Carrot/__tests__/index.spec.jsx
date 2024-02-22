2baa5ce4d7a24657ca0af4b80c158c2eb40614a9
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-02-22 14:18:38 -0500

    927-Icon-component: remove carrot component

--- benefit-finder/src/shared/components/Carrot/__tests__/index.spec.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import Carrot from '../index.jsx'
-
-describe('Carrot', () => {
-  it('renders a match to the previous snapshot', () => {
-    const { asFragment } = render(<Carrot />)
-    expect(asFragment()).toMatchSnapshot()
-  })
-})

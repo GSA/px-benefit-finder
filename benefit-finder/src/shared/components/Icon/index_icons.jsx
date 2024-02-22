72e80d53c80fdecf642a431f1e87cb70b1790f74
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-02-22 15:02:39 -0500

    927-Icon-component: include info icon

--- benefit-finder/src/shared/components/Icon/index_icons.jsx
+++ benefit-finder/src/shared/components/Icon/index_icons.jsx
@@ -61,6 +61,30 @@ export const GreenCheck = () => (
   </svg>
 )
 
+export const InfoIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="USWDS Components">
+      <g id="Icons">
+        <g id="Fill">
+          <path
+            id="Vector"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9 5H11V7H9V5ZM9 9H11V15H9V9ZM10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18Z"
+            fill="black"
+          />
+        </g>
+      </g>
+    </g>
+  </svg>
+)
+
 export const ModalClose = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -85,4 +109,12 @@ export const Open = () => (
   </svg>
 )
 
-export default { CarrotBig, CarrotSmall, Close, GreenCheck, ModalClose, Open }
+export default {
+  CarrotBig,
+  CarrotSmall,
+  Close,
+  GreenCheck,
+  InfoIcon,
+  ModalClose,
+  Open,
+}

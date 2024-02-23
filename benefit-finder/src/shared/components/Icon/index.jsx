ddba09f1501b283638214bccf507369c658ff611
gscottqueen <scott.queen@contractor.bixal.com>
2024-02-23 14:39:34 -0500

    751-relative-benefits-icons: wrap icon return with semantic element

--- benefit-finder/src/shared/components/Icon/index.jsx
+++ benefit-finder/src/shared/components/Icon/index.jsx
@@ -2,32 +2,53 @@ import PropTypes from 'prop-types'
 import * as Icons from './index_icons'
 import './_index.scss'
 
+/**
+ * a functional component that renders a svg icon
+ * @component
+ * @param {string} type - string kabob value of component name
+ * @param {string} color - hex or value
+ * @param {any} props - inherited props
+ * @return {html} returns a semantic <i> element with <svg> inside
+ */
 const Icon = ({ type, color, ...props }) => {
   let icon
 
   switch (type) {
     case 'carrot-small':
-      icon = <Icons.CarrotSmall color={color} {...props} />
+      icon = <Icons.CarrotSmall color={color} />
       break
     case 'carrot-big':
-      icon = <Icons.CarrotBig {...props} />
+      icon = <Icons.CarrotBig />
+      break
+    case 'death':
+      icon = <Icons.Death />
+      break
+    case 'disability':
+      icon = <Icons.Disability />
       break
     case 'close':
-      icon = <Icons.Close {...props} />
+      icon = <Icons.Close />
       break
     case 'green-check':
-      icon = <Icons.GreenCheck {...props} />
+      icon = <Icons.GreenCheck />
       break
     case 'open':
-      icon = <Icons.Open {...props} />
+      icon = <Icons.Open />
       break
     case 'modal-close':
-      icon = <Icons.ModalClose {...props} />
+      icon = <Icons.ModalClose />
+      break
+    case 'retirement':
+      icon = <Icons.Retirement />
       break
     default:
       icon = null
   }
-  return icon
+  return (
+    <i {...props} aria-hidden="true">
+      {icon}
+    </i>
+  )
 }
 
 Icon.propTypes = {

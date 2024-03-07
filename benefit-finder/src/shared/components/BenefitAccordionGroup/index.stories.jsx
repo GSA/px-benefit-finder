b1dea5997bd213060497bd64101450577b367b74
gscottqueen <scott.queen@contractor.bixal.com>
2024-03-07 07:20:31 -0500

    PXBF-976-unit-coverage-in-english-modifier: new story with source is english example

--- benefit-finder/src/shared/components/BenefitAccordionGroup/index.stories.jsx
+++ benefit-finder/src/shared/components/BenefitAccordionGroup/index.stories.jsx
@@ -1,28 +1,48 @@
 import BenefitAccordionGroup from './index.jsx'
 import content from '../../api/mock-data/current.js'
 import * as en from '../../locales/en/en.json'
+import * as es from '../../locales/es/es.json'
 
 const { data } = JSON.parse(content)
 const { benefits } = data
-const b = [benefits[0], benefits[1]]
+const b = benefits.slice(0, 7)
 const entryKey = Object.keys(b[0])
 const { resultsView } = en
+
+const SourceIsEnglishBenefits = b.slice(5, 7)
+SourceIsEnglishBenefits[0].benefit.SourceIsEnglish = 'TRUE' // ensure true
+SourceIsEnglishBenefits[1].benefit.SourceIsEnglish = 'FALSE' // ensure false
 
 export default {
   component: BenefitAccordionGroup,
   tags: ['autodocs'],
   args: {
-    data: b,
+    data: b.slice(0, 2),
     entryKey: entryKey[0],
     ui: resultsView,
   },
 }
 
-export const Primary = {}
+export const Primary = {
+  args: {
+    data: b.slice(1, 3),
+  },
+}
 
 export const ExpandAll = {
   args: {
     ...Primary.args,
+    data: b.slice(3, 5),
+    expandAll: true,
+  },
+}
+
+// we only indicate sourceIsEnglish in es locale
+export const SourceIsEnglish = {
+  args: {
+    ...Primary.args,
+    data: SourceIsEnglishBenefits,
+    ui: es.resultsView,
     expandAll: true,
   },
 }

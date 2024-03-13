2d7e5c736ca667b30777dae14892f7a45a7051d7
gscottqueen <scott.queen@contractor.bixal.com>
2024-03-13 09:58:39 -0400

    879-cypress-e2e-usagov-static-site: exclude public site in default config

--- benefit-finder/cypress.config.js
+++ benefit-finder/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:6006',
+    excludeSpecPattern: 'cypress/e2e/usagov-public-site/*.cy.js',
   },
   component: {
     devServer: {

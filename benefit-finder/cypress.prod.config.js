bc13cc735b4374f026d9df3d64a55db7a697b7a0
gscottqueen <scott.queen@contractor.bixal.com>
2024-03-13 11:17:26 -0400

    879-cypress-e2e-usagov-static-site: only run public site tests with prod config for now

--- benefit-finder/cypress.prod.config.js
+++ benefit-finder/cypress.prod.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  video: true,
   e2e: {
     baseUrl: 'https://www.usa.gov',
+    specPattern: 'cypress/e2e/usagov-public-site/*.cy.js',
   },
 })

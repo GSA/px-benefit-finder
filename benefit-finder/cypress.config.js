21909c73fba224bbf8f2eccf8cbde1ca5ef9659d
gscottqueen <scott.queen@contractor.bixal.com>
2024-03-14 16:59:44 -0400

    PXBF-980-cypress-axe: configure logger for cypress-axe

--- benefit-finder/cypress.config.js
+++ benefit-finder/cypress.config.js
@@ -1,9 +1,25 @@
+/* eslint-disable no-console */
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:6006',
     excludeSpecPattern: 'cypress/e2e/usagov-public-site/*.cy.js',
+    defaultCommandTimeout: 8000,
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+
+          return null
+        },
+        table(message) {
+          console.table(message)
+
+          return null
+        },
+      })
+    },
   },
   component: {
     devServer: {

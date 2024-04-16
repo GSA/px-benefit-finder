a85a88756ad76bf568f03450fb7c1393e85beb29
Nehemiah Abuga <121119072+nehemiah-abuga@users.noreply.github.com>
2024-04-15 20:42:29 -0700

    PXBF-1091-fix-nightly-run-fails: suppress errors on usagov benefit finder

--- benefit-finder/cypress/e2e/usagov-public-site/benefits-page-navigate-benefits-finder-card.cy.js
+++ benefit-finder/cypress/e2e/usagov-public-site/benefits-page-navigate-benefits-finder-card.cy.js
@@ -3,6 +3,9 @@
 import { pageObjects } from '../../support/pageObjects'
 
 describe('Validate benefit-finder card on benefits page', () => {
+  Cypress.on('uncaught:exception', (_err, runnable) => {
+    return false
+  })
   it('Should navigate to Benefit finder when clicking on Benefit finder card', () => {
     cy.visit('/benefits')
     pageObjects.cardGroup().contains('Benefit finder').click()

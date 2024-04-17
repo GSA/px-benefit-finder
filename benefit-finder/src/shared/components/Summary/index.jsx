756b291a1b69ec9188e5d0ee0ffbdcb7da6f6929
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-04-17 12:58:25 -0400

    PXBF-1196-summary-box-component: new summary component

---
+++ benefit-finder/src/shared/components/Summary/index.jsx
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types'
+import { Heading } from '../index'
+import './_index.scss'
+
+const Summary = ({ heading, listItems, cta }) => {
+  const handleReload = e => {
+    e.preventDefault()
+    window.location.href = `${window.location.origin}${window.location.pathname}`
+  }
+
+  return (
+    <div
+      className="bf-usa-summary-box usa-summary-box"
+      role="region"
+      aria-labelledby="bf-summary-box-key-information summary-box-key-information"
+    >
+      <div className="bf-usa-summary-box__body usa-summary-box__body">
+        <Heading
+          headingLevel={4}
+          className="bf-usa-summary-box__heading usa-summary-box__heading"
+          id="bf-summary-box-key-information"
+        >
+          {heading}
+        </Heading>
+        <div className="bf-usa-summary-box__text usa-summary-box__text">
+          <ul className="bf-usa-list usa-list">
+            {listItems &&
+              listItems.map((item, i) => (
+                <li key={`bf-summary-list-${i}`}>{item.item}</li>
+              ))}
+            <li>
+              {cta.text}{' '}
+              <a
+                className="bf-usa-summary-box__link usa-summary-box__link"
+                href="#"
+                onClick={e => handleReload(e)}
+              >
+                {cta.link}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Summary.propTypes = {
+  props: PropTypes.any,
+}
+
+export default Summary

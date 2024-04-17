f16146fe6fb6da207968dbec5853b273d32496e2
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-04-17 10:01:59 -0400

    PXBF-clean-up-release: clean up mv script

--- scripts/pipeline/mv-usagov_benefit_finder.sh
+++ scripts/pipeline/mv-usagov_benefit_finder.sh
@@ -14,20 +14,20 @@ readonly USAGOV_PROJECT_CUSTOM_MODULES_LOCATION="${USAGOV_PROJECT_LOCATION}/web/
 # check if usagov-2021 custom modules directory exist
 if test ! -d "$USAGOV_PROJECT_CUSTOM_MODULES_LOCATION"
 then
-    echo "\xE2\x9C\x94 usa.gov project directory does not exists, initializing submodule"
+    echo "usa.gov project directory does not exists, initializing submodule"
     # init submodule
     git submodule init
     git submodule update
 else
-    echo "\xE2\x9C\x94 usa.gov project directory exists, updating submodule"
+    echo "usa.gov project directory exists, updating submodule"
     git pull --recurse-submodules
 fi
 
 # check if module directory exist
 if test -d "$BENEFIT_FINDER_MODULE_LOCATION"
 then
-    echo "\xE2\x9C\x94 usa.gov benefit-finder module exist"
+    echo "usa.gov benefit-finder module exist"
     echo "Moving BENEFIT_FINDER custom module to usa.gov project..."
     cp -r "${BENEFIT_FINDER_MODULE_LOCATION}" "${USAGOV_PROJECT_CUSTOM_MODULES_LOCATION}" || exit 1
-    test -f "${USAGOV_PROJECT_CUSTOM_MODULES_LOCATION}${BENEFIT_FINDER_MODULE}"; echo -e "\xE2\x9C\x94 BENEFIT_FINDER Module successfully moved"
+    test -f "${USAGOV_PROJECT_CUSTOM_MODULES_LOCATION}${BENEFIT_FINDER_MODULE}"; echo -e "BENEFIT_FINDER Module successfully moved"
 fi

32b6f411a665b334b6aeaace1a162f7c913794bf
Nehemiah Abuga <121119072+nehemiah-abuga@users.noreply.github.com>
2024-04-28 20:45:41 -0700

    PXBF-1204-test-coverage-remove-skip-fail-on-error: remove skipFailOnError

--- benefit-finder/cypress/e2e/storybook/axe-a11y.cy.js
+++ benefit-finder/cypress/e2e/storybook/axe-a11y.cy.js
@@ -35,15 +35,14 @@ const status =
     .fieldsets[2].fieldset.inputs[0].inputCriteria.values[1].value
 
 describe(`Validate code passes axe scanning`, () => {
-  const runA11y = (skipFailOnError = null) => {
+  const runA11y = () => {
     cy.checkA11y(
       null,
       {
         retries: 3,
         interval: 100,
       },
-      terminalLog,
-      skipFailOnError
+      terminalLog
     )
   }
 
@@ -68,7 +67,7 @@ describe(`Validate code passes axe scanning`, () => {
   it('Has no detectable a11y violations on error state', () => {
     pageObjects.button().contains(EN_LOCALE_DATA.intro.button).click()
     pageObjects.button().contains(EN_LOCALE_DATA.buttonGroup[1].value).click()
-    runA11y({ skipFailOnError: true }) // remove skip after https://github.com/GSA/px-benefit-finder/issues/1072 is resolved
+    runA11y()
   })
 
   it('Has no detectable a11y violations on error state resolved', () => {
@@ -133,6 +132,6 @@ describe(`Validate code passes axe scanning`, () => {
       .button()
       .contains(EN_LOCALE_DATA.resultsView.notEligibleResults.cta)
       .click()
-    runA11y({ skipFailOnError: true }) // remove skip after https://github.com/GSA/px-benefit-finder/issues/1073 is resolved
+    runA11y()
   })
 })

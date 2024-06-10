b34e6f3efa45d2ca194ab69a7d875a53bba99c13
Xavier Metichecchia <XavierMetichecchia>
2024-06-10 09:51:46 -0400

    Service key fix try 2

--- scripts/pipeline/cloud-gov-cleanup-service-keys.sh
+++ scripts/pipeline/cloud-gov-cleanup-service-keys.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-service="${PROJECT}-backup-${ENVIRONMENT}"
+service="${PROJECT}-backup-${BRANCH}"
 
 raw_keys=$(cf service-keys "${service}" | sed 1,3d | awk '{print $1}')
 service_keys=("${raw_keys}")
@@ -11,6 +11,6 @@ echo "Cleaning up service keys..."
 {
   for key in "${service_keys[@]}"; do
     echo "Deleting service key '${key}'..."
-    cf delete-service-key "${service}" "${key}" -f >/dev/null 2>&1
+    cf delete-service-key "${service}" "${key}" -f #>/dev/null 2>&1
   done
-} >/dev/null 2>&1
+} #>/dev/null 2>&1

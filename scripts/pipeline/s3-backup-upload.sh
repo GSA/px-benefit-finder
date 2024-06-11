85a2c85e949a92c46f95bdaf94687ef8430ab377
Xavier Metichecchia <XavierMetichecchia>
2024-06-11 11:26:11 -0400

    Cleanup

--- scripts/pipeline/s3-backup-upload.sh
+++ scripts/pipeline/s3-backup-upload.sh
@@ -6,7 +6,7 @@ backup_bucket="${PROJECT}-backup-${BRANCH}"
 space="${PROJECT}-${BRANCH}"
 
 echo "Getting backup bucket credentials..."
-# {
+ {
   cf target -s "${space}"
 
   service="${backup_bucket}"
@@ -17,7 +17,7 @@ echo "Getting backup bucket credentials..."
   s3_credentials=$(cf service-key "${service}" "${service_key}" | tail -n +2)
 
   AWS_ACCESS_KEY_ID=$(echo "${s3_credentials}" | jq -r '.credentials.access_key_id')
-  echo "AWS_ACCESS_KEY_ID IS: ${AWS_ACCESS_KEY_ID}"
+
   export AWS_ACCESS_KEY_ID
   
   bucket=$(echo "${s3_credentials}" | jq -r '.credentials.bucket')
@@ -32,11 +32,11 @@ echo "Getting backup bucket credentials..."
   AWS_SECRET_ACCESS_KEY=$(echo "${s3_credentials}" | jq -r '.credentials.secret_access_key')
   export AWS_SECRET_ACCESS_KEY
 
-# } &> /dev/null
+ } &> /dev/null
 
 
 echo "Checking AWS credentials..."
-# {
+ {
   start_time=$(date +%s)
   while ! aws sts get-caller-identity > /dev/null 2>&1; do
     echo "AWS credentials not valid yet, retrying in 15 seconds..."
@@ -49,15 +49,15 @@ echo "Checking AWS credentials..."
     fi
   done
   echo "Credentials Verified, Proceeding with Upload"
-# } &> /dev/null
+ } 
 
 echo "Uploading backup..."
-# {
+ {
 
   aws s3 cp "${TIMESTAMP}.sql.gz" "s3://${bucket}/$(date +%Y)/$(date +%m)/$(date +%d)/" --no-verify-ssl # 2>/dev/null
   aws s3 cp "${TIMESTAMP}.sql.gz" "s3://${bucket}/latest.sql.gz" --no-verify-ssl # 2>/dev/null
   cf delete-service-key "${service}" "${service_key}" -f
 
-# } &> /dev/null
+ } &> /dev/null
 
 echo "File uploaded: $(date +%Y)/$(date +%m)/$(date +%d)/${TIMESTAMP}.sql.gz"

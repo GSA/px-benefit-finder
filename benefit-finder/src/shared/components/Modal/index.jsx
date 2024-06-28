e47181caef9c6c034c86a56eb47bd0c2400bb804
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-06-28 18:08:45 -0400

    PXBF-1511-error-count: pass new async function to modal handler

--- benefit-finder/src/shared/components/Modal/index.jsx
+++ benefit-finder/src/shared/components/Modal/index.jsx
@@ -39,6 +39,7 @@ const customStyles = {
  * @param {string} modalHeading - heading value
  * @param {string} navItemOneLabel - passed to button for nav item in modal
  * @param {string} navItemTwoLabel - passed to button for nav item in modal
+ * @param {function} handleCheckRequriedFields - inherited async function to check validity of fields
  * @return {html} returns html for seting up a usa-modal component
  */
 const Modal = ({
@@ -62,10 +63,12 @@ const Modal = ({
    * @function
    */
   const handleOpenModal = () => {
-    if (handleCheckRequriedFields() === true) {
-      scrollLock.enableScroll()
-      setModalOpen(true)
-    }
+    handleCheckRequriedFields().then(valid => {
+      if (valid === true) {
+        scrollLock.enableScroll()
+        setModalOpen(true)
+      }
+    })
   }
 
   /**
@@ -242,6 +245,7 @@ Modal.propTypes = {
   navItemOneFunction: PropTypes.func,
   navItemTwoLabel: PropTypes.string,
   navItemTwoFunction: PropTypes.func,
+  handleCheckRequriedFields: PropTypes.func,
 }
 
 export default Modal

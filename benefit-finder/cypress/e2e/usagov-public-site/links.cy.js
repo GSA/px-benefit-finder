61d7e082e6b0487006c694ace6433f43980ee6a5
gscottqueen <scott.queen@bixal.com>
2024-07-01 17:43:14 -0400

    PXBF-1392-handle-codes-prod-link-test: set up negative validations before we run tests

--- benefit-finder/cypress/e2e/usagov-public-site/links.cy.js
+++ benefit-finder/cypress/e2e/usagov-public-site/links.cy.js
@@ -14,24 +14,31 @@ const localePaths = {
   ],
 }
 
-const validateErrorCodes = () => {
+const handlerequest = ({ testLink, link }) => {
+  return cy
+    .request({
+      url: testLink || link.prop('href'),
+      failOnStatusCode: false,
+    })
+    .then(response => {
+      if (response.status === 200) {
+        expect(response.status).to.eq(200)
+      } else if (response.status === 403) {
+        cy.get('body').children().its('length').should('be.gt', 0)
+      } else if (response.status === 503) {
+        throw new Error('site down - gave a 503')
+      } else if (response.status === 404) {
+        throw new Error('page not found - gave a 404')
+      } else {
+        cy.get('body').children().its('length').should('be.gt', 0)
+      }
+    })
+}
+
+const validateErrorCodes = test => {
   // we verify site is alive and fail on 404 || 503
   cy.get('#benefit-finder a[href]').each(link => {
-    cy.request({ url: link.prop('href'), failOnStatusCode: false }).then(
-      response => {
-        if (response.status === 200) {
-          expect(response.status).to.eq(200)
-        } else if (response.status === 403) {
-          cy.get('body').children().its('length').should('be.gt', 0)
-        } else if (response.status === 503) {
-          throw new Error('site down - gave a 503')
-        } else if (response.status === 404) {
-          throw new Error('page not found - gave a 404')
-        } else {
-          cy.get('body').children().its('length').should('be.gt', 0)
-        }
-      }
-    )
+    handlerequest({ link })
   })
 }
 
@@ -41,6 +48,35 @@ const validateLinks = ({ selectedData, path }) => {
   cy.visit(`${path}?${scenario}`)
   validateErrorCodes()
 }
+
+describe('Verify correct status code handling', () => {
+  // negate validation on our code function
+  it(`handles 404 with an error`, () => {
+    Cypress.on('fail', error => {
+      expect(error).to.not.be.undefined
+    })
+    handlerequest({ testLink: 'https://httpstat.us/404' })
+  })
+
+  it(`handles 503 with an error`, () => {
+    Cypress.on('fail', error => {
+      expect(error).to.not.be.undefined
+    })
+    handlerequest({ testLink: 'https://httpstat.us/503' })
+  })
+
+  it(`handles 200 successfully`, () => {
+    handlerequest({ testLink: 'https://httpstat.us/200' })
+  })
+
+  it(`handles any 403 successfully`, () => {
+    handlerequest({ testLink: 'https://httpstat.us/403' })
+  })
+
+  it(`handles any other request successfully`, () => {
+    handlerequest({ testLink: 'https://httpstat.us/201' })
+  })
+})
 
 describe('Verify correct status code when user navigates links in each locales', () => {
   // to be removed when uncaught exceptions are addressed

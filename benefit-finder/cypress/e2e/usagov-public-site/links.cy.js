dcaf3883f4ff2bc62ff630ae3e93553ea29fd05f
gscottqueen <scott.queen@bixal.com>
2024-07-01 18:17:14 -0400

    PXBF-1392-handle-codes-prod-link-test: only ignore errors when meaning to, and include url with thrown errors

--- benefit-finder/cypress/e2e/usagov-public-site/links.cy.js
+++ benefit-finder/cypress/e2e/usagov-public-site/links.cy.js
@@ -15,9 +15,10 @@ const localePaths = {
 }
 
 const handlerequest = ({ testLink, link }) => {
+  const url = testLink || link.prop('href')
   return cy
     .request({
-      url: testLink || link.prop('href'),
+      url,
       failOnStatusCode: false,
     })
     .then(response => {
@@ -26,9 +27,9 @@ const handlerequest = ({ testLink, link }) => {
       } else if (response.status === 403) {
         cy.get('body').children().its('length').should('be.gt', 0)
       } else if (response.status === 503) {
-        throw new Error('site down - gave a 503')
+        throw new Error(`site down - gave a 503 ${url}`)
       } else if (response.status === 404) {
-        throw new Error('page not found - gave a 404')
+        throw new Error(`page not found - gave a 404 ${url}`)
       } else {
         cy.get('body').children().its('length').should('be.gt', 0)
       }
@@ -49,19 +50,26 @@ const validateLinks = ({ selectedData, path }) => {
   validateErrorCodes()
 }
 
+// to be removed when uncaught exceptions are addressed
+Cypress.on('uncaught:exception', (error, runnable) => {
+  return false
+})
+
 describe('Verify correct status code handling', () => {
-  // negate validation on our code function
-  it(`handles 404 with an error`, () => {
-    Cypress.on('fail', error => {
+  // negate validation on our functional code
+  Cypress.on('fail', (error, runnable) => {
+    if (JSON.stringify(error).includes('httpstat')) {
       expect(error).to.not.be.undefined
-    })
+    } else {
+      throw error
+    }
+  })
+
+  it(`handles 404 with an error`, () => {
     handlerequest({ testLink: 'https://httpstat.us/404' })
   })
 
   it(`handles 503 with an error`, () => {
-    Cypress.on('fail', error => {
-      expect(error).to.not.be.undefined
-    })
     handlerequest({ testLink: 'https://httpstat.us/503' })
   })
 
@@ -79,11 +87,6 @@ describe('Verify correct status code handling', () => {
 })
 
 describe('Verify correct status code when user navigates links in each locales', () => {
-  // to be removed when uncaught exceptions are addressed
-  Cypress.on('uncaught:exception', (_err, runnable) => {
-    return false
-  })
-
   localePaths.en.forEach(location => {
     it(`Verify success status code response for links in ${location.key} en page`, () => {
       validateLinks({

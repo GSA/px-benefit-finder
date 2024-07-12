7625ea5dd157c13836881dbe5a75dc5e99d3c383
gscottqueen <scott.queen@bixal.com>
2024-07-12 16:29:57 -0400

    PXBF-1562-restructure-results-view-b: new heading blocks variants for results view

--- benefit-finder/src/shared/components/ResultsView/components/index.js
+++ benefit-finder/src/shared/components/ResultsView/components/index.js
@@ -1,1 +1,3 @@
+export { default as EligibleBenefitsHeadingBlock } from './EligibleBenefitsHeadingBlock'
+export { default as NotEligibleBenefitsHeadingBlock } from './NotEligibleBenefitsHeadingBlock'
 export { default as ZeroBenefitsHeadingBlock } from './ZeroBenefitsHeadingBlock'

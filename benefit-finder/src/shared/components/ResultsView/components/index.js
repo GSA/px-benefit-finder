70eeb66b40cd3204941b739c77f4c7e4fe78d599
gscottqueen <scott.queen@bixal.com>
2024-07-12 17:02:50 -0400

    PXBF-1562-restructure-results-view-b: new ResultsViewBlock component

--- benefit-finder/src/shared/components/ResultsView/components/index.js
+++ benefit-finder/src/shared/components/ResultsView/components/index.js
@@ -1,3 +1,4 @@
 export { default as EligibleBenefitsHeadingBlock } from './EligibleBenefitsHeadingBlock'
 export { default as NotEligibleBenefitsHeadingBlock } from './NotEligibleBenefitsHeadingBlock'
+export { default as ResultsViewBlock } from './ResultsViewBlock'
 export { default as ZeroBenefitsHeadingBlock } from './ZeroBenefitsHeadingBlock'

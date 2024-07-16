5608b579ab6eb679aa099c68e2da72e1b62e6994
scottqueen-bixal <scott.queen@bixal.com>
2024-07-16 13:32:37 -0400

    PXBF-1562-restructure-results-view-b: place back other tests in this suite

--- benefit-finder/cypress/e2e/storybook/benefitAccordionGroup.cy.js
+++ benefit-finder/cypress/e2e/storybook/benefitAccordionGroup.cy.js
@@ -8,13 +8,13 @@ beforeEach(() => {
 })
 
 describe('BenefitAccordionGroup component tests', () => {
-  // it.only('Validate all accordions are closed on load', () => {
-  //   cy.get('.bf-usa-accordion:visible').each(accordion => {
-  //     cy.wrap(accordion)
-  //       .find('.bf-usa-accordion__button')
-  //       .should('have.attr', 'aria-expanded', 'false')
-  //   })
-  // })
+  it('Validate all accordions are closed on load', () => {
+    cy.get('.bf-usa-accordion:visible').each(accordion => {
+      cy.wrap(accordion)
+        .find('.bf-usa-accordion__button')
+        .should('have.attr', 'aria-expanded', 'false')
+    })
+  })
 
   it('Validate opening individual accordion expands the clicked accordion and clicking it again closes it', () => {
     cy.wait(2500)
@@ -40,49 +40,24 @@ describe('BenefitAccordionGroup component tests', () => {
       .should('have.attr', 'aria-expanded', 'false')
   })
 
-  // it('Validate clicking Expand all opens all accordions', () => {
-  //   cy.get('.bf-expand-all').click()
-  //   cy.get('.bf-expand-all').should('contain.text', 'Close all')
-  //   cy.get('.bf-usa-accordion:visible').each(accordion => {
-  //     cy.wrap(accordion)
-  //       .find('.bf-usa-accordion__button')
-  //       .should('have.attr', 'aria-expanded', 'true')
-  //   })
-  // })
+  it('Validate clicking Expand all opens all accordions', () => {
+    cy.get('.bf-expand-all').click()
+    cy.get('.bf-expand-all').should('contain.text', 'Close all')
+    cy.get('.bf-usa-accordion:visible').each(accordion => {
+      cy.wrap(accordion)
+        .find('.bf-usa-accordion__button')
+        .should('have.attr', 'aria-expanded', 'true')
+    })
+  })
 
-  // it('Validate clicking Collapse all closes all accordions', () => {
-  //   cy.get('.bf-expand-all').click()
-  //   cy.get('.bf-expand-all').click()
-  //   cy.get('.bf-expand-all').should('contain.text', 'Open all')
-  //   cy.get('.bf-usa-accordion:visible').each(accordion => {
-  //     cy.wrap(accordion)
-  //       .find('.bf-usa-accordion__button')
-  //       .should('have.attr', 'aria-expanded', 'false')
-  //   })
-  // })
+  it('Validate clicking Collapse all closes all accordions', () => {
+    cy.get('.bf-expand-all').click()
+    cy.get('.bf-expand-all').click()
+    cy.get('.bf-expand-all').should('contain.text', 'Open all')
+    cy.get('.bf-usa-accordion:visible').each(accordion => {
+      cy.wrap(accordion)
+        .find('.bf-usa-accordion__button')
+        .should('have.attr', 'aria-expanded', 'false')
+    })
+  })
 })
-
-// breif update
-
-// outside of work
-// new art project using midjouney and programiatic AI image generation and apply them to a mechanical drawing process
-
-// benefit finder
-// prepareing for a release
-// collecting technical requirements for new feature requests
-// troubleshooting some cypress failures that are only occuring in the pipeline, "works on my local"!
-
-// what I want to learn
-
-// What I am leaning now
-
-// got my kicks on front end development
-// but, more full stack experience, back end languages, postgress (db) super powers
-// currently going deep in python, formal training
-
-// What I want to learn
-// more depth in traditional computer science knowledge, especially on how it relates to advanced visiualization techniques, in data visualization or virtual and augmented environments
-
-// unlimitied time and resource
-
-// more space exploriing

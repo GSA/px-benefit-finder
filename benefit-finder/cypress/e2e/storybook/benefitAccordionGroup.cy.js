5320873788fb12571c6b4f1dc41ee75f73f5bc53
scottqueen-bixal <scott.queen@bixal.com>
2024-07-16 13:23:51 -0400

    PXBF-1562-restructure-results-view-b: clock back to half wait

--- benefit-finder/cypress/e2e/storybook/benefitAccordionGroup.cy.js
+++ benefit-finder/cypress/e2e/storybook/benefitAccordionGroup.cy.js
@@ -17,9 +17,7 @@ describe('BenefitAccordionGroup component tests', () => {
   // })
 
   it('Validate opening individual accordion expands the clicked accordion and clicking it again closes it', () => {
-    cy.get('.bf-usa-accordion:visible .bf-usa-accordion__button')
-      .eq(0)
-      .should('be.visible')
+    cy.wait(2500)
     // get the first visible accordion and check if it is expanded
     cy.get('.bf-usa-accordion:visible .bf-usa-accordion__button')
       .eq(0)
@@ -63,3 +61,28 @@ describe('BenefitAccordionGroup component tests', () => {
   //   })
   // })
 })
+
+// breif update
+
+// outside of work
+// new art project using midjouney and programiatic AI image generation and apply them to a mechanical drawing process
+
+// benefit finder
+// prepareing for a release
+// collecting technical requirements for new feature requests
+// troubleshooting some cypress failures that are only occuring in the pipeline, "works on my local"!
+
+// what I want to learn
+
+// What I am leaning now
+
+// got my kicks on front end development
+// but, more full stack experience, back end languages, postgress (db) super powers
+// currently going deep in python, formal training
+
+// What I want to learn
+// more depth in traditional computer science knowledge, especially on how it relates to advanced visiualization techniques, in data visualization or virtual and augmented environments
+
+// unlimitied time and resource
+
+// more space exploriing

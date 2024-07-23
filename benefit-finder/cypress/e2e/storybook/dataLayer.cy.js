576985e81326dc4b05ce3736b06e42cb21565ec2
scottqueen-bixal <scott.queen@bixal.com>
2024-07-23 17:06:14 -0400

    PXBF-1548-update-gtm-e2e-tests: force click

--- benefit-finder/cypress/e2e/storybook/dataLayer.cy.js
+++ benefit-finder/cypress/e2e/storybook/dataLayer.cy.js
@@ -435,7 +435,7 @@ describe('Calls to Google Analytics Object', function () {
     })
   })
 
-  it.only('clicking a obfuscated link in an open accordion on the results page with eligible benefits has a bf_benefit_link event', function () {
+  it('clicking a obfuscated link in an open accordion on the results page with eligible benefits has a bf_benefit_link event', function () {
     cy.visit(`${utils.storybookUri}${scenario}`)
 
     cy.window().then(window => {
@@ -468,7 +468,7 @@ describe('Calls to Google Analytics Object', function () {
             pageObjects
               .benefitsAccordionLink(enResults.eligible.eligible_benefits[0])
               .invoke('removeAttr', 'href')
-              .click()
+              .click({ force: true })
               .then(() => {
                 // get all the events in our layer that matches the event value
                 const ev = [

7c6f89ce8532b3803c83751caab0356434a0d376
Xavier Metichecchia <XavierMetichecchia>
2024-07-25 15:04:54 -0400

    PXBF-dev: Setting dev stuff back, checking main 10

--- scripts/pipeline/github-update-issue-status.sh
+++ scripts/pipeline/github-update-issue-status.sh
@@ -143,6 +143,6 @@ echo "Updating issue project fields..."
     --id "${gh_issue_id}" \
     --field-id "${gh_priority_id}" \
     --single-select-option-id "${gh_priority_option_id}"
-} >
+} 
 
 echo "Update completed."

3e66c4bdc1dc8243e1eeea8f10746bf89978a120
scottqueen-bixal <scott.queen@bixal.com>
2024-07-31 15:40:47 -0400

    PXBF-1635-init-script: updated echos

--- scripts/local/init.sh
+++ scripts/local/init.sh
@@ -4,13 +4,24 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 # set up githooks
-echo("Set githook config to point at custom hooks")
+echo "Set githook config to point at custom hooks"
 git config --local core.hooksPath .githooks/
 
-echo("Install usagov-2021 submodule")
+echo "Install usagov-2021 git submodule"
 # set up gitsubmodule
 git submodule init
 git submodule update
 
-echo("setup our benefit finder dependencies and containers")
-bash `${ROOT_DIR}/scripts/local/setup-usagov-benefit-finder.sh`
+# init usagov-2021
+echo "init usagov-2021"
+cd "${ROOT_DIR}/usagov-2021"
+bin/init
+
+# install node dependencies
+echo "install benefit-finder node dependencies"
+cd "${ROOT_DIR}/benefit-finder"
+npm install
+touch .env.local
+echo "export VITE_PROXY_URL=\"https://bf-cms-dev.bxdev.net\"" >> .env.local
+
+

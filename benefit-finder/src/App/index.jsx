ac54b917cc105744dd3414410e5336371d5bc28b
scottqueen-bixal <scott.queen@bixal.com>
2024-09-24 09:34:52 -0400

    set up initial routes on app component

--- benefit-finder/src/App/index.jsx
+++ benefit-finder/src/App/index.jsx
@@ -1,4 +1,5 @@
 import { useState, createContext, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { version } from '../../package.json'
 import { useResetElement } from '@hooks'
 import * as apiCalls from '@api/apiCalls'
@@ -72,13 +73,30 @@ function App({ testAppContent, testQuery }) {
 
   // state
   const [t] = useState(apiCalls.GET.Language() === 'es' ? es : en) // tranlations
-  const [step, setStep] = useState(hasQueryParams ? null : 0) // steps indicator
 
+  // handle step, stepData, and stepVerification
+  const [step, setStep] = useState(hasQueryParams ? null : 0) // steps indicator
   const [stepData, setStepData] = useState(
     () => stepDataArray && stepDataArray[step]
   ) // content
   const [verifyStep, setVerifyStep] = useState(false) // verification view
+
+  // handle new view layout for results
   const [viewResults, setViewResults] = useState(hasQueryParams) // resuts view
+
+  // set up defualts for route
+  const basePath = '/benefit-finder/'
+  const indexPath = content?.lifeEventForm.id
+
+  useEffect(() => {
+    if (
+      indexPath &&
+      !hasQueryParams &&
+      window.location.pathname !== `${basePath}${indexPath}`
+    ) {
+      window.location = `${basePath}${indexPath}`
+    }
+  }, [indexPath])
 
   useEffect(() => {
     if (hasQueryParams) {
@@ -94,7 +112,8 @@ function App({ testAppContent, testQuery }) {
   }, [windowQuery, hasQueryParams, stepDataArray])
 
   return (
-    content && (
+    content &&
+    stepDataArray && (
       <LanguageContext.Provider value={t}>
         {isDraftMode === true && <Alert>Draft Mode</Alert>}
         <div
@@ -105,69 +124,187 @@ function App({ testAppContent, testQuery }) {
           data-testid="app"
           data-version={version}
         >
-          {step === 0 ? (
-            <Intro
-              data={content.lifeEventForm}
-              ui={t.intro}
-              setStep={setStep}
-              step={step}
-            />
-          ) : viewResults === true ? (
-            <ResultsView
-              stepDataArray={stepDataArray}
-              relevantBenefits={content?.lifeEventForm?.relevantBenefits}
-              data={benfitsArray}
-              setBenefitsArray={() => setBenefitsArray()}
-              ui={t.resultsView}
-              handleStepBack={() => {
-                setVerifyStep(false)
-                setViewResults(false)
-              }}
-            />
-          ) : verifyStep === false ? (
-            <div>
-              <Form>
-                <LifeEventSection
-                  step={step}
-                  setStep={setStep}
-                  data={stepDataArray}
-                  handleData={setStepDataArray}
-                  stepData={stepData}
-                  setStepData={setStepData}
-                  verifyStep={verifyStep}
-                  setVerifyStep={() => {
-                    setVerifyStep(true)
-                    setModalOpen(false)
-                  }}
-                  setViewResults={() => {
-                    setViewResults(true)
-                    setModalOpen(false)
-                  }}
-                  ui={t}
-                  modalOpen={modalOpen}
-                  setModalOpen={setModalOpen}
-                />
-              </Form>
-            </div>
-          ) : (
-            <VerifySelectionsView
-              handleStepBack={() => {
-                setVerifyStep(false)
-                setViewResults(false)
-              }}
-              handleStepForward={() => {
-                setViewResults(true)
-              }}
-              ui={t}
-              data={stepDataArray}
-              step={step}
-              setStep={setStep}
-            />
-          )}
+          <BrowserRouter basename={basePath}>
+            <Routes>
+              <Route
+                path={`/${indexPath}`}
+                element={
+                  stepDataArray && (
+                    <Intro
+                      content={content.lifeEventForm}
+                      ui={t.intro}
+                      setStep={setStep}
+                      step={step}
+                      stepDataArray={stepDataArray}
+                      indexPath={`/${indexPath}/`}
+                    />
+                  )
+                }
+              />
+              {stepDataArray &&
+                stepDataArray.map((item, i) => {
+                  const path = item.section.heading
+                    .toLowerCase()
+                    .replace(/ /g, '-')
+                  return (
+                    <Route
+                      path={`/${indexPath}/${path}`}
+                      key={i}
+                      element={
+                        <div>
+                          <Form>
+                            <LifeEventSection
+                              step={step}
+                              setStep={setStep}
+                              data={stepDataArray}
+                              handleData={setStepDataArray}
+                              stepData={stepData}
+                              setStepData={setStepData}
+                              indexPath={`/${indexPath}/`}
+                              verifyStep={verifyStep}
+                              setVerifyStep={() => {
+                                setVerifyStep(true)
+                                setModalOpen(false)
+                              }}
+                              setViewResults={() => {
+                                setViewResults(true)
+                                setModalOpen(false)
+                              }}
+                              ui={t}
+                              modalOpen={modalOpen}
+                              setModalOpen={setModalOpen}
+                            />
+                          </Form>
+                        </div>
+                      }
+                    />
+                  )
+                })}
+              <Route
+                path={`/${indexPath}/verify-selections`}
+                element={
+                  <VerifySelectionsView
+                    handleStepBack={() => {
+                      setVerifyStep(false)
+                      setViewResults(false)
+                    }}
+                    handleStepForward={() => {
+                      setViewResults(true)
+                    }}
+                    ui={t}
+                    data={stepDataArray}
+                    step={step}
+                    setStep={setStep}
+                    indexPath={indexPath}
+                  />
+                }
+              />
+              <Route
+                path={`/${indexPath}/results`}
+                element={
+                  <ResultsView
+                    stepDataArray={stepDataArray}
+                    relevantBenefits={content?.lifeEventForm?.relevantBenefits}
+                    data={benfitsArray}
+                    setBenefitsArray={() => setBenefitsArray()}
+                    ui={t.resultsView}
+                    handleStepBack={() => {
+                      setVerifyStep(false)
+                      setViewResults(false)
+                    }}
+                  />
+                }
+              />
+            </Routes>
+          </BrowserRouter>
         </div>
       </LanguageContext.Provider>
     )
   )
 }
+
+// return (
+//   content && (
+//     <LanguageContext.Provider value={t}>
+//       {isDraftMode === true && <Alert>Draft Mode</Alert>}
+//       <div
+//         id={content?.lifeEventForm.id}
+//         className={`benefit-finder ${
+//           step !== 0 && viewResults !== true ? 'form' : ''
+//         }`}
+//         data-testid="app"
+//         data-version={version}
+//       >
+//         {step === 0 ? (
+//           <Intro
+//             data={content.lifeEventForm}
+//             ui={t.intro}
+//             setStep={setStep}
+//             step={step}
+//             stepPath={
+//               stepDataArray &&
+//               stepDataArray.length > 0 &&
+//               stepDataArray[step].section.heading
+//                 .toLowerCase()
+//                 .replace(/ /g, '-')
+//             }
+//           />
+//         ) : viewResults === true ? (
+//           <ResultsView
+//             stepDataArray={stepDataArray}
+//             relevantBenefits={content?.lifeEventForm?.relevantBenefits}
+//             data={benfitsArray}
+//             setBenefitsArray={() => setBenefitsArray()}
+//             ui={t.resultsView}
+//             handleStepBack={() => {
+//               setVerifyStep(false)
+//               setViewResults(false)
+//             }}
+//           />
+//         ) : verifyStep === false ? (
+//           <div>
+//             <Form>
+//               <LifeEventSection
+//                 step={step}
+//                 setStep={setStep}
+//                 data={stepDataArray}
+//                 handleData={setStepDataArray}
+//                 stepData={stepData}
+//                 setStepData={setStepData}
+//                 verifyStep={verifyStep}
+//                 setVerifyStep={() => {
+//                   setVerifyStep(true)
+//                   setModalOpen(false)
+//                 }}
+//                 setViewResults={() => {
+//                   setViewResults(true)
+//                   setModalOpen(false)
+//                 }}
+//                 ui={t}
+//                 modalOpen={modalOpen}
+//                 setModalOpen={setModalOpen}
+//               />
+//             </Form>
+//           </div>
+//         ) : (
+//           <VerifySelectionsView
+//             handleStepBack={() => {
+//               setVerifyStep(false)
+//               setViewResults(false)
+//             }}
+//             handleStepForward={() => {
+//               setViewResults(true)
+//             }}
+//             ui={t}
+//             data={stepDataArray}
+//             step={step}
+//             setStep={setStep}
+//           />
+//         )}
+//       </div>
+//     </LanguageContext.Provider>
+//   )
+// )
+// }
 
 export default App

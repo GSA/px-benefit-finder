f98f4d4eea4555f1a2d36fa5ba1439cad07f5ac2
scottqueen-bixal <scott.queen@bixal.com>
2024-09-30 14:52:11 -0400

    wrap zero benefits snap with browser router

--- benefit-finder/src/shared/components/ResultsView/components/blocks/ZeroBenefitsHeading/__tests__/index.spec.jsx
+++ benefit-finder/src/shared/components/ResultsView/components/blocks/ZeroBenefitsHeading/__tests__/index.spec.jsx
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import ZeroBenefitsHeading from '../index.jsx'
 import * as en from '@locales/en/en.json'
 
 describe('ZeroBenefitsHeading', () => {
   it('renders a match to the previous snapshot', () => {
-    const { asFragment } = render(<ZeroBenefitsHeading ui={en} />)
+    const { asFragment } = render(<ZeroBenefitsHeading ui={en} />, {
+      wrapper: BrowserRouter,
+    })
+
     expect(asFragment()).toMatchSnapshot()
   })
 })

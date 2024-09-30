371b50270b01386bca530dc06bdebafef160e211
scottqueen-bixal <scott.queen@bixal.com>
2024-09-30 14:51:25 -0400

    wrap verify step-indicator snap with browser router

--- benefit-finder/src/shared/components/StepIndicator/__tests__/index.spec.jsx
+++ benefit-finder/src/shared/components/StepIndicator/__tests__/index.spec.jsx
@@ -1,9 +1,25 @@
 import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import StepIndicator from '../index.jsx'
+import content from '@api/mock-data/current.js'
+// import * as en from '@locales/en/en.json'
+
+const { data } = JSON.parse(content)
+const { lifeEventForm } = data
 
 describe('StepIndicator', () => {
   it('renders a match to the previous snapshot', () => {
-    const { asFragment } = render(<StepIndicator />)
+    const { asFragment } = render(
+      <StepIndicator
+        current={1}
+        // setCurrent={setFormStep}
+        data={lifeEventForm.sectionsEligibilityCriteria}
+        // key={`step-indicator-${sectionHeadings}`}
+      />,
+      {
+        wrapper: BrowserRouter,
+      }
+    )
     expect(asFragment()).toMatchSnapshot()
   })
 })

6696e3bbf108561a9f0e829642eff011ec9699c4
Xavier Metichecchia <XavierMetichecchia>
2024-10-21 14:52:53 -0400

    PXBF-main: Reset tome scripts to main URI without 1

--- scripts/application/bf-tome-run.sh
+++ scripts/application/bf-tome-run.sh
@@ -5,6 +5,7 @@ SCRIPT_NAME=$(basename "$0")
 SCRIPT_PID=$$
 
 URI=${1:-https://bf-static-main.bxdev.net}
+URI=https://bf-static-main.bxdev.net
 # URI=https://bf-static-dev.bxdev.net
 FORCE=${2:-0}
 RETRY_SEMAPHORE_FILE=/tmp/tome-log/retry-on-next-run

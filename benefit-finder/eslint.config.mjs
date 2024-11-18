6343b43e4076d4986ea5be1775d6544c3ee908b9
scottqueen-bixal <scott.queen@bixal.com>
2024-11-18 15:45:53 -0500

    PXBF-1955-update-lint-config: include jsx a11y rules

--- benefit-finder/eslint.config.mjs
+++ benefit-finder/eslint.config.mjs
@@ -22,6 +22,7 @@ export default [
   ...storybook.configs['flat/recommended'],
   pluginCypress.configs.recommended,
   reactPlugin.configs.flat.all,
+  jsxA11y.flatConfigs.recommended,
   {
     files: ['**/*.js', '**/*.mjs'],
     languageOptions: {
@@ -110,7 +111,11 @@ export default [
         ...GLOBALS_BROWSER_FIX,
       },
     },
-    rules: {},
+    rules: {
+      'jsx-a11y/no-noninteractive-tabindex': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      'jsx-a11y/mouse-events-have-key-events': 'off',
+    },
   },
   {
     ignores: [

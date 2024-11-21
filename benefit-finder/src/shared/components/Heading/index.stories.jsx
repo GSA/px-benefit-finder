2601341c63b0cea206f6cc50c23f56ef15a38527
scottqueen-bixal <scott.queen@bixal.com>
2024-11-21 17:17:15 -0500

    PXBF-1954-mobile-layout-updates: updated heading stories

--- benefit-finder/src/shared/components/Heading/index.stories.jsx
+++ benefit-finder/src/shared/components/Heading/index.stories.jsx
@@ -22,35 +22,21 @@ export const Default = {}
 
 export const Heading1 = {
   args: {
-    children: '32 pt H1 Heading 1 -  Sample',
+    children: 'H1 Heading 1 -  Sample',
     headingLevel: 1,
   },
 }
 
 export const Heading2 = {
   args: {
-    children: '24 pt H2 Heading 2 -  Sample',
+    children: 'H2 Heading 2 -  Sample',
     headingLevel: 2,
   },
 }
 
 export const Heading3 = {
   args: {
-    children: '22 pt H3 Heading 3 -  Sample',
+    children: 'H3 Heading 3 -  Sample',
     headingLevel: 3,
-  },
-}
-
-export const Heading4 = {
-  args: {
-    children: '20 pt H4 Heading 4 -  Sample',
-    headingLevel: 4,
-  },
-}
-
-export const Heading5 = {
-  args: {
-    children: '16 pt H5 Heading 5 -  Sample',
-    headingLevel: 5,
   },
 }

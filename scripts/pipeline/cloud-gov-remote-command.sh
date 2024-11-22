a6044b2fbb418e86d0865da3405b54888f4b7863
Xavier Metichecchia <XavierMetichecchia>
2024-11-22 15:22:53 -0500

    Debug main WAF failure cleanup

--- scripts/pipeline/cloud-gov-remote-command.sh
+++ scripts/pipeline/cloud-gov-remote-command.sh
@@ -8,15 +8,7 @@ APP_GUID=$(cf app "${APP_NAME}" --guid)
 bin_path="/var/www/vendor/bin/:/home/vcap/deps/0/bin/"
 
 
-### Start Remove After Testing
-if [ -z "${APP_NAME}" ] || [ -z "${command}" ]; then
-  echo "Command error! Valid format: ${0} <application_name> <command>"
-  exit 1
-fi
-### End Remove After Testing
-
-###Uncomment after testing
-# [ -z "${APP_NAME}" ] || [ -z "${command}" ] && echo "Command error! Valid format: ${0} <application_name> <command>" && exit 1
+[ -z "${APP_NAME}" ] || [ -z "${command}" ] && echo "Command error! Valid format: ${0} <application_name> <command>" && exit 1
 
 ssh_config=/tmp/ssh_config
 ssh_passwd="/tmp/ssh_password"
@@ -30,33 +22,7 @@ Host ssh.fr.cloud.gov
   RequestTTY force
 EOL
 
-# echo "Fetching SSH code..." ### Remove Line after testing
 cf ssh-code > ${ssh_passwd}
-
-### Start Remove After Testing
-# Enhanced troubleshooting: Always show detailed output
-# echo "Executing command via SSH: ${command}"
-# output=$(sshpass -f "${ssh_passwd}" ssh -F "${ssh_config}" "ssh.fr.cloud.gov" "touch ~/.bashrc && source ~/.bashrc && PATH=\$PATH:${bin_path} ${command}" 2>&1)
-#
-# # Capture the exit code
-# exit_code=$?
-#
-# Log output and errors
-# if [ $exit_code -ne 0 ]; then
-#   echo "Error encountered during SSH command execution."
-#   echo "Command: ${command}"
-#   echo "Output:"
-#   echo "${output}"
-#   echo "Exit Code: ${exit_code}"
-# else
-#   echo "Command executed successfully. Output:"
-#   echo "${output}"
-# fi
-#
-# exit $exit_code
-### End Remove After Testing
-
-###Uncomment after testing
 
 if [ -z "${show_output}" ]; then
   echo "Running command: '$(echo "${command}" | cut -d' ' -f1,2)'..."

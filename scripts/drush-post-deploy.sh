e01d2e52a1db0ff620a3888419a48bb3f7b4b7b9
Xavier Metichecchia <XavierMetichecchia>
2024-12-11 11:59:20 -0500

    Revert recent work for release

--- scripts/drush-post-deploy.sh
+++ scripts/drush-post-deploy.sh
@@ -3,19 +3,11 @@
 ## Make sure the last line has a new line or the pipeline script won't read it.
 ## It reads lines based on new lines.
 
-echo "Updating Drupal..."
-drush cr
+echo  "Updating drupal ... "
 drush state:set system.maintenance_mode 1 -y
 drush cr
 drush updatedb --no-cache-clear -y
 drush cim --partial --source=modules/custom/usagov_benefit_finder/configuration -y
 drush cr
-drush user:create test_test --password="TU_PASS"
-drush upwd --password="TU_PASS" gene_chi
-drush upwd --password="TU_PASS" scott_queen
-drush upwd --password="TU_PASS" nehemia_abuga
-drush upwd --password="TU_PASS" diego_cob
-drush upwd --password="TU_PASS" cindy_fong
-drush upwd --password="TU_PASS" ernie_deeb
 drush state:set system.maintenance_mode 0 -y
-echo "Post-deploy finished!"
+echo "Post deploy finished!"

0ff749f0f6d50c2d72db702044768514bbcbc459
Xavier Metichecchia <XavierMetichecchia>
2024-12-12 16:36:07 -0500

    Mute the deploy script, rebuild the drush script with names from secrets 5

--- scripts/drush-post-deploy.sh
+++ scripts/drush-post-deploy.sh
@@ -9,16 +9,7 @@ drush cr
 drush updatedb --no-cache-clear -y
 drush cim --partial --source=modules/custom/usagov_benefit_finder/configuration -y
 drush cr
-
-# Placeholder for user password reset commands
 ### USER_PASSWORD_RESET_PLACEHOLDER ###
-
-# drush user:password gene_chi "TU_PASS"
-# drush user:password scott_queen "TU_PASS"
-# drush user:password nehemia_abuga "TU_PASS"
-# drush user:password diego_cob "TU_PASS"
-# drush user:password cindy_fong "TU_PASS"
-# drush user:password ernie_deeb "TU_PASS"
 drush state:set system.maintenance_mode 0 -y
 drush user:create test_test --password="TU_PASS" || true
 drush user:create xtest_test --password="TU_PASS" || true

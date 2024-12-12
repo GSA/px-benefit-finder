d7f41b67410d9e29738f8152b3c16986576654d5
Xavier Metichecchia <XavierMetichecchia>
2024-12-12 18:12:12 -0500

    Mute again

--- scripts/pipeline/cloud-gov-post-deploy.sh
+++ scripts/pipeline/cloud-gov-post-deploy.sh
@@ -6,25 +6,25 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 # Read the drush-post-deploy.sh file line by line
 while IFS= read -r command || [[ -n "$command" ]]; do
   # Debugging: Print the current command being processed
-  echo "Processing command: ${command}"
+  # echo "Processing command: ${command}"
 
   # Skip comments, empty lines, or echo commands
   if [[ "${command}" != "#"* ]] && [[ -n "${command}" ]] && [[ "${command}" != "echo "* ]]; then
-    echo "Running command: ${command}"
+    # echo "Running command: ${command}"
     
     # Call the cloud-gov-remote-command script with the project and branch
-    bash ./scripts/pipeline/cloud-gov-remote-command.sh "${PROJECT}-cms-${BRANCH}" "${command}" < /dev/null #>/dev/null 2>&1
+    bash ./scripts/pipeline/cloud-gov-remote-command.sh "${PROJECT}-cms-${BRANCH}" "${command}" < /dev/null >/dev/null 2>&1
 
     # Check for errors and exit if a command fails
     if [[ $? -ne 0 ]]; then
-      echo "Error: Command '${command}' failed!" #>&2
+      # echo "Error: Command '${command}' failed!" #>&2
       exit 1
     fi
   fi
 done < <(cat "${ROOT_DIR}/scripts/drush-post-deploy.sh")
 
 # Indicate script completion
-echo "All commands processed successfully!"
+# echo "All commands processed successfully!"
 exit 0
 
 #Casey's Original Script

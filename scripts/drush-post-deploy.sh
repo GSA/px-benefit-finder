b0eea7402510a9814743cce3f36a35a23e1224db
Xavier Metichecchia <XavierMetichecchia>
2024-12-12 11:41:31 -0500

    Check all command make order changes

--- scripts/drush-post-deploy.sh
+++ scripts/drush-post-deploy.sh
@@ -9,13 +9,13 @@ drush cr
 drush updatedb --no-cache-clear -y
 drush cim --partial --source=modules/custom/usagov_benefit_finder/configuration -y
 drush cr
+drush user:password gene_chi "TU_PASS"
+drush user:password scott_queen "TU_PASS"
+drush user:password nehemia_abuga "TU_PASS"
+drush user:password diego_cob "TU_PASS"
+drush user:password cindy_fong "TU_PASS"
+drush user:password ernie_deeb "TU_PASS"
 drush state:set system.maintenance_mode 0 -y
-drush user:create test_test --password="TU_PASS"
-drush upwd --password="TU_PASS" gene_chi
-drush upwd --password="TU_PASS" scott_queen
-drush upwd --password="TU_PASS" nehemia_abuga
-drush upwd --password="TU_PASS" diego_cob
-drush upwd --password="TU_PASS" cindy_fong
-drush upwd --password="TU_PASS" ernie_deeb
-drush user:create xtest_test --password="TU_PASS"
+drush user:create test_test --password="TU_PASS" || true
+drush user:create xtest_test --password="TU_PASS" || true
 echo "Post deploy finished!"

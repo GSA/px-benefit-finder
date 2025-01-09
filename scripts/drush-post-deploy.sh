bbb8858518b2ecbe22b35089d162bf0a0ae7b202
Xavier Metichecchia <XavierMetichecchia>
2025-01-09 13:38:08 -0500

    Remove --pipe from drush command to remove usagov login

--- scripts/drush-post-deploy.sh
+++ scripts/drush-post-deploy.sh
@@ -12,5 +12,5 @@ drush cr
 ### USER_PASSWORD_RESET_PLACEHOLDER ###
 drush state:set system.maintenance_mode 0 -y
 # drush pm:uninstall usagov_login --strict=0
-drush pm-list --type=module --pipe | grep -q "^usagov_login " && drush pm-uninstall -y usagov_login || echo "Module 'usagov_login' does not exist. Skipping uninstallation."
+drush pm-list --type=module --status=enabled | grep -q "^usagov_login " && drush pm-uninstall -y usagov_login || echo "Module 'usagov_login' does not exist. Skipping uninstallation."
 echo "Post deploy finished!"
